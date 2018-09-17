089741bb2c62f4751e479bf785a40feb78ad4cbb
Matias Trujillo <mtrujillo@outlook.cl>
2018-09-17 03:46:14 -0300

    setAttribute now works as a communicator between frameworks

--- src/element.js
+++ src/element.js
@@ -10,37 +10,20 @@ export default class extends HTMLElement {
         this[ELEMENT] = true;
         this.state = {};
         this.slots = {};
+        this.props = {};
+        this.fragment = document.createDocumentFragment();
         this._props = this.constructor.props || [];
         this._render = [];
-        this.props = getProps(this._props, this);
-        this.props.children = [];
-        this.fragment = document.createDocumentFragment();
-        while (this.firstChild) {
-            let child = this.firstChild,
-                slot = child.getAttribute && child.getAttribute("slot");
-            if (slot) {
-                this.slots[slot] = child;
-            }
-            append(this.fragment, child);
-            this.props.children.push(child);
-        }
+        this._mount;
         this.livecycle();
     }
-
     static get observedAttributes() {
         return this.props || [];
     }
     livecycle() {
-        let prevent = true;
         this.listeners = [MOUNT, UNMOUNT, RECEIVE_PROPS].map(type => {
             let handler = event => {
-                if (event.type !== type) return;
-
-                if (type === MOUNT) {
-                    prevent = false;
-                }
-
-                if (prevent) return;
+                if (!this._mount) return;
 
                 if (this[type]) this[type](event);
 
@@ -61,12 +44,34 @@ export default class extends HTMLElement {
     }
     setAttribute(prop, value) {
         if (this._props.indexOf(prop) > -1) {
-            this.attributeChangedCallback(prop, this.props[prop], value);
+            if (this._mount) {
+                this.attributeChangedCallback(prop, this.props[prop], value);
+            } else {
+                getProps([prop], { [prop]: value }, this.props);
+            }
         } else {
             super.setAttribute(prop, value);
         }
     }
+    /**
+     * By default the children and properties are extracted
+     * only when the component exists in the document
+     * This is required for the component to be read regardless
+     * of the load instance
+     */
     connectedCallback() {
+        this._mount = true;
+        this.props = { ...getProps(this._props, this), ...this.props };
+        this.props.children = [];
+        while (this.firstChild) {
+            let child = this.firstChild,
+                slot = child.getAttribute && child.getAttribute("slot");
+            if (slot) {
+                this.slots[slot] = child;
+            }
+            append(this.fragment, child);
+            this.props.children.push(child);
+        }
         this.dispatch(MOUNT);
     }
     disconnectedCallback() {
