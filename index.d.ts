fca6fd9b446e94319656051e761be21790374dfd
Matias Trujillo <mtrujillo@outlook.cl>
2018-09-19 14:33:29 -0300

    update

--- index.d.ts
+++ index.d.ts
@@ -1,13 +1,30 @@
 import VDom from "./src/vdom";
 
+interface Slots{
+    [key:string] : HTMLElement | SVGAElement
+}
+
+type Tag = String | HTMLElement | SVGAElement;
+
 declare module "atomico"{
-    export function h(tag:string,attrs:Object,...children:any[] ):VDom
+    export function h(tag:Tag,attrs:Object,...children:any):VDom
     export class Component extends HTMLElement{
         state:Object;
         props:Object;
         _props:string[];
         _render:VDom[];
-        dispatch(type:string,detail:any):void
-        setState(next:Object):void
+        _prevent:boolean;
+        _mount:boolean;
+        fragment : document.createDocumentFragment;
+        slots : Slots;
+        setAttribute(props:string,value:any):void;
+        connectedCallback():void;
+        disconnectedCallback():void;
+        attributeChangedCallback():void;
+        addEventListener(type:string,handler:Function,useCapture?:boolean):void;
+        dispatch(type:string,detail:any):void;
+        setState(next:Object,ignoreUpdate?:boolean):void;
+        setProps(props:Object):void;
+        render():Any;
     }
 }
