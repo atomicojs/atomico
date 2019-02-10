c93e4901f410c01671db3f2c99788d2d6e3f689a
Matias Trujillo <mtrujillo@outlook.cl>
2019-02-09 22:30:29 -0300

    If you do not execute tasks after 60 frames, the frameLoop is cleaned

--- src/task.js
+++ src/task.js
@@ -1,21 +1,36 @@
 let taks = [],
-    autorun;
+    inFrameLoop;
 /**
  * creates a task loop that is not affected by other external interval effects or nested intervals
  * @param {function} callback
+ * @todo please test in hot-reload
  */
 export function createTask(callback) {
     taks.push(callback);
-    if (!autorun) {
-        autorun = true;
-        requestAnimationFrame(function loop() {
-            setTimeout(() => {
-                let currentTasks = taks,
-                    length = currentTasks.length;
-                taks = [];
-                for (let i = 0; i < length; i++) currentTasks[i]();
+    if (!inFrameLoop) {
+        inFrameLoop = true;
+        let cancel = () => {
+                cancelAnimationFrame(frameLoop);
+                inFrameLoop = false;
+            },
+            count = 0,
+            frameLoop = requestAnimationFrame(function loop() {
+                setTimeout(() => {
+                    let currentTasks = taks,
+                        length = currentTasks.length;
+                    taks = [];
+                    if (!length) {
+                        count++;
+                    } else {
+                        count = 0;
+                    }
+                    if (count === 60) {
+                        cancel();
+                    } else {
+                        for (let i = 0; i < length; i++) currentTasks[i]();
+                        requestAnimationFrame(loop);
+                    }
+                });
             });
-            requestAnimationFrame(loop);
-        });
     }
 }
