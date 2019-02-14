9fe52c347d34b149dc454b1c1b8e4a15b3f521db
Matias Trujillo <mtrujillo@outlook.cl>
2019-02-14 11:56:27 -0300

    now task use messageChanel

--- src/task.js
+++ src/task.js
@@ -1,36 +1,38 @@
-let taks = [],
-    inFrameLoop;
+let task = [],
+    currentLoop,
+    frameEmpty = 0,
+    channel = new MessageChannel();
+
+channel.port1.onmessage = () => {
+    let currentTask = task,
+        length = currentTask.length;
+    if (!length) {
+        frameEmpty++;
+    } else {
+        frameEmpty = 0;
+    }
+    for (let i = 0; i < length; i++) currentTask[i]();
+    task = [];
+};
+
+function createLoop() {
+    function loop() {
+        if (frameEmpty > 60) {
+            currentLoop = 0;
+        } else {
+            channel.port2.postMessage(undefined);
+            requestAnimationFrame(loop);
+        }
+    }
+    currentLoop = requestAnimationFrame(loop);
+}
+
 /**
  * creates a task loop that is not affected by other external interval effects or nested intervals
  * @param {function} callback
  * @todo please test in hot-reload
  */
 export function createTask(callback) {
-    taks.push(callback);
-    if (!inFrameLoop) {
-        inFrameLoop = true;
-        let cancel = () => {
-                cancelAnimationFrame(frameLoop);
-                inFrameLoop = false;
-            },
-            count = 0,
-            frameLoop = requestAnimationFrame(function loop() {
-                setTimeout(() => {
-                    let currentTasks = taks,
-                        length = currentTasks.length;
-                    taks = [];
-                    if (!length) {
-                        count++;
-                    } else {
-                        count = 0;
-                    }
-                    if (count === 60) {
-                        cancel();
-                    } else {
-                        for (let i = 0; i < length; i++) currentTasks[i]();
-                        requestAnimationFrame(loop);
-                    }
-                });
-            });
-    }
+    task.push(callback);
+    if (!currentLoop) createLoop();
 }
