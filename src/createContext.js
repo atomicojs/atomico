aa683a7ba96861f9470f65e19f8d77dbb943c194
Matias Trujillo <mtrujillo@outlook.cl>
2019-02-14 01:13:37 -0300

    add createContext

--- src/createContext.js
+++ src/createContext.js
@@ -1,21 +1,35 @@
 import { vnode } from "./vnode";
 import { CONTEXT } from "./constants";
 
-let counter = 0;
-
+// incremental id to identify contexts
+let counterID = 0;
+/**
+ * allows the manipulation of the context based on the reading of ShadowContext
+ * @param {array}
+ * @return {object}
+ */
 function ShadowContext({ children }) {
     return children[0];
 }
-
-export function createContext() {
-    let space = CONTEXT + "." + counter++;
-
+/**
+ * create a context with a reserved space name at the time of the instance
+ * This name is not unique, it is based on the @context.${counterID++}.
+ */
+export function createContext(valueDefault) {
+    let space = CONTEXT + "." + counterID++;
+    // manipulate the context property using ShadowContext as a proxy component
     function Provider({ value, children }) {
-        return vnode(ShadowContext, { [space]: value }, children);
+        return vnode(
+            ShadowContext,
+            { context: { [space]: value || valueDefault } },
+            children
+        );
     }
-    function Consumer({ props, children }, context) {
+    // consumes the context from context based on the name of space
+    function Consumer({ children }, context) {
         return children[0](context[space]);
     }
+    // returns the context name of the context
     function toString() {
         return space;
     }
