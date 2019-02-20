07b3d803004b10648c83bb8a497de9ac3d3c9918
Matias Trujillo <mtrujillo@outlook.cl>
2019-02-20 19:48:43 -0300

    the process of reading components is rewritten, now this peritme check if the component requires an update or not

--- src/component.js
+++ src/component.js
@@ -1,45 +1,28 @@
-import { isArray, isEqualArray } from "./utils";
-import { options } from "./options";
-import { update } from "./update";
-import { createTask } from "./task";
-
+import { setTask } from "./task";
 import {
     COMPONENT_CREATE,
-    COMPONENT_CREATED,
     COMPONENT_UPDATE,
+    COMPONENT_CREATED,
     COMPONENT_UPDATED,
-    COMPONENT_REMOVE,
-    COMPONENT_CLEAR
+    COMPONENT_REMOVE
 } from "./constants";
 
-let CURRENT_COMPONENT;
-let CURRENT_KEY_HOOKS;
+import { update as updateNode } from "./update";
 
-export function getCurrentComponent() {
-    if (!CURRENT_COMPONENT) {
+let CURRENT_SNAP, CURRENT_SNAP_KEY_HOOK;
+
+export function getCurrentSnap() {
+    if (!CURRENT_SNAP) {
         throw new Error(
             "the hooks can only be called from an existing functional component in the diff queue"
         );
     }
-    return CURRENT_COMPONENT;
-}
-
-export function useRender() {
-    let component = getCurrentComponent();
-    return () => {
-        if (!component.prevent) {
-            component.prevent = true;
-            createTask(() => {
-                component.render();
-                component.prevent = false;
-            });
-        }
-    };
+    return CURRENT_SNAP;
 }
 
 export function useHook(reducer) {
-    let component = getCurrentComponent(),
-        index = CURRENT_KEY_HOOKS++,
+    let component = getCurrentSnap().component,
+        index = CURRENT_SNAP_KEY_HOOK++,
         hook,
         isCreate;
     if (!component.hooks[index]) {
@@ -52,73 +35,168 @@ export function useHook(reducer) {
     return [hook.state, action => dispatchHook(hook, action)];
 }
 
-export function clearComponentEffects(components, isRemove) {
-    let length = components.length;
-    for (let i = 0; i < length; i++) {
-        components[i].dispatch(isRemove ? COMPONENT_REMOVE : COMPONENT_CLEAR);
-    }
-}
-
 export function dispatchHook(hook, action) {
     hook.state = hook.reducer(hook.state, action);
 }
 
-export function createComponent(tag, isHost, isSvg, deep, components) {
-    let isRemove,
-        hooks = [],
-        isCreate = true;
-
-    function dispatch(type) {
-        if (isRemove) return;
-        if (type === COMPONENT_REMOVE) {
-            isRemove = true;
+export function dispatchComponents(components, action) {
+    let length = components.length;
+    for (let i = 0; i < length; i++) {
+        let component = components[i],
+            hooks = component.hooks,
+            hooksLength = hooks.length;
+        if (action.type === COMPONENT_REMOVE) {
+            component.remove = true;
         }
-        let action = { type },
-            length = hooks.length;
-        for (let i = 0; i < length; i++) dispatchHook(hooks[i], action);
+        for (let i = 0; i < hooksLength; i++) {
+            dispatchHook(hooks[i], action);
+        }
     }
+}
+/**
+ * this function allows creating a block that analyzes the tag
+ * defined as a function, in turn creates a global update scope for hook management.
+ */
+export function createUpdateComponent(ID, isSvg) {
+    let prevent,
+        store = [],
+        host;
+    /**
+     * This function allows reducing the functional components based on
+     * their return, in turn creates a unique state for each component
+     * according to a depth index
+     * @param {function} vnode
+     * @param {object} context
+     * @param {number} deep
+     */
+    function reduce(vnode, context, deep) {
+        // if host does not exist as a node, the vnode is not reduced
+        if (!host) return;
+        vnode = vnode || "";
+        // if it is different from a functional node, it is sent to updateNode again
+        if (typeof vnode.tag !== "function") {
+            dispatchComponents(store.splice(deep), {
+                type: COMPONENT_REMOVE
+            });
+            host = updateNode(ID, host, vnode, isSvg, context, updateComponent);
+            // if the store no longer has a length, it is assumed that the updateComponent is no longer necessary
+            if (store.length) host[ID].updateComponent = updateComponent;
 
-    function render() {
-        if (isRemove) return;
+            return;
+        }
+        // you get the current component
+        let component = store[deep] || {},
+            isCreate,
+            useNext;
+        // if the current component is dis- torted to the analyzed one,
+        // the previous state is replaced with a new one and the elimination is dispatched.
+        if (component.tag !== vnode.tag) {
+            isCreate = true;
+            // the state of the component is defined
+            store[deep] = {
+                lvl: 1,
+                size: 1,
+                tag: vnode.tag,
+                hooks: [],
+                props: {}
+            };
+            // the elimination is sent to the successors of the previous component
+            dispatchComponents(store.splice(deep + 1), {
+                type: COMPONENT_REMOVE
+            });
+            useNext = true;
+        }
 
-        CURRENT_KEY_HOOKS = 0;
-        CURRENT_COMPONENT = component;
+        component = store[deep];
+        // then a series of simple processes are carried out capable of
+        // identifying if the component requires an update
+        if (component.context !== vnode.useContext) {
+            // the current context is stored in the cache
+            component.context = vnode.useContext;
+            // create a new context
+            context = { ...context, ...vnode.useContext };
+            useNext = true;
+        }
 
-        dispatch(isCreate ? COMPONENT_CREATE : COMPONENT_UPDATE);
-
-        let vnode = tag(component.props, component.context);
-
-        CURRENT_KEY_HOOKS = 0;
-        CURRENT_COMPONENT = false;
-
-        let base = update(
-            component.base,
-            vnode,
-            isHost,
-            isSvg,
-            component.context,
-            deep + 1,
-            components
-        );
-
-        if (base !== component.base) {
-            for (let i = 0; i < deep; i++) {
-                components[i].base = base;
+        if (!useNext) {
+            // compare the lake of properties
+            if (vnode.size !== component.size) useNext = true;
+            if (!useNext) {
+                // buy property by property, so the properties to be used
+                // in the areas must be immutable
+                for (let key in vnode.props) {
+                    if (vnode.props[key] !== component.props[key]) {
+                        useNext = true;
+                        break;
+                    }
+                }
             }
         }
 
-        dispatch(isCreate ? COMPONENT_CREATED : COMPONENT_UPDATED);
+        component.props = vnode.props;
+        component.size = vnode.size;
+        /**
+         * this function is a snapshot of the current component,
+         * allows to run the component and launch the next update
+         */
+        function next() {
+            if (component.remove) return host;
 
-        isCreate = false;
+            CURRENT_SNAP = {
+                component,
+                context,
+                // allows access to the instantaneous, but it uses the microtareas
+                // to prevent multiple synchronous updates
+                next() {
+                    if (!component.prevent) {
+                        component.prevent = true;
+                        setTask(() => {
+                            component.prevent = false;
+                            next();
+                        });
+                    }
+                }
+            };
 
-        return (component.base = base);
+            CURRENT_SNAP_KEY_HOOK = 0;
+
+            dispatchComponents([component], { type: COMPONENT_UPDATE });
+
+            let vnextnode = component.tag(component.props, context);
+
+            CURRENT_SNAP = false;
+            CURRENT_SNAP_KEY_HOOK = 0;
+
+            reduce(vnextnode, context, deep + 1);
+
+            dispatchComponents([component], {
+                type: isCreate ? COMPONENT_CREATED : COMPONENT_UPDATED
+            });
+
+            isCreate = false;
+        }
+        if (useNext) next();
+    }
+    /**
+     *
+     * @param {string} type
+     * @param {HTMLElement|SVGElement|Text} nextHost
+     * @param {object} vnode
+     * @param {object} context
+     */
+    function updateComponent(type, nextHost, vnode, context) {
+        switch (type) {
+            case COMPONENT_UPDATE:
+                host = nextHost;
+                reduce(vnode, context, 0);
+                return host;
+            case COMPONENT_REMOVE:
+                host = false;
+                dispatchComponents(history, { type });
+                history = [];
+                break;
+        }
     }
 
-    let component = {
-        tag,
-        hooks,
-        render,
-        dispatch
-    };
-    return component;
+    return updateComponent;
 }
