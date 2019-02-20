de7ecda706da7ae88f71418bcd141da10c1e7571
Matias Trujillo <mtrujillo@outlook.cl>
2019-02-20 19:59:43 -0300

    it is rewritten

--- src/update.js
+++ src/update.js
@@ -1,142 +1,133 @@
-import { createComponent, clearComponentEffects } from "./component";
-import { updateElement, defineNodeTag } from "./updateElement";
-import { updateChildren, clearElement } from "./updateChildren";
-import { updateProperties } from "./updateProperties";
+import { NODE_TEXT, NODE_HOST, TAG_VALUE, COMPONENT_UPDATE } from "./constants";
+import { options } from "./options";
 import { defineVnode } from "./vnode";
-import { STATE, STATE_HOST, NODE_TEXT, NODE_HOST } from "./constants";
+import { updateChildren } from "./updateChildren";
+import { updateProperties } from "./updateProperties";
+import { createUpdateComponent } from "./component";
+import { setTask } from "./task";
+import { createVnode } from "./vnode";
 /**
- * updates a node based on the state of the vnode
- * @param {HTMLElement|SVGAElement|Text} [prevNode] - if false update returns a new node
- * @param {object|text|number} vnode - define next node state
- * @param {boolean} isSvg - define if the tree is of type svg
- * @param {object} context - current context
- * @param {number} [deep] - High order component depth
- * @param {array} [components] - List of high-order components associated with the node
- * @return {HTMLElement|SVGAElement|Text}
+ * @param {object} vnode
+ * @param {HTMLElement|SVGElement} node
+ * @param {string} [customID]
+ * @param {boolean} disableHost
+ */
+export function render(vnode, node, customID = "vstate", disableHost) {
+    if (!disableHost) {
+        vnode = defineVnode(vnode);
+        if (vnode.tag !== NODE_HOST) {
+            vnode = createVnode(NODE_HOST, {}, [vnode]);
+        }
+    }
+    update(customID, node, vnode);
+}
+
+export function createNode(tag, isSvg) {
+    let doc = options.document || document,
+        nextNode;
+    if (tag !== NODE_TEXT) {
+        nextNode = isSvg
+            ? doc.createElementNS("http://www.w3.org/2000/svg", tag)
+            : doc.createElement(tag);
+    } else {
+        nextNode = doc.createTextNode("");
+    }
+    nextNode[TAG_VALUE] = tag;
+    return nextNode;
+}
+
+export function getNodeName(node) {
+    if (!node) return;
+    // store the process locally in the node to avoid transformation
+    if (!node[TAG_VALUE]) {
+        node[TAG_VALUE] = node.nodeName.toLowerCase();
+    }
+    return node[TAG_VALUE];
+}
+/**
+ *
+ * @param {string} ID - store the process locally in the node to avoid transformation
+ * @param {HTMLElement|SVGElement|Text|undefined} prevNode - if the current node is defined and the next
+ * one to be used is different, the replacement of the current node will be made
+ * @param {*} vnode
+ * @param {boolean} isSvg
+ * @param {object} context
+ * @param {function|undefined} currentUpdateComponent
  */
 export function update(
+    ID,
     prevNode,
     vnode,
-    isHost,
     isSvg,
-    context = {},
-    deep = 0,
-    components = []
+    context,
+    currentUpdateComponent
 ) {
-    // check if the vnode is valid
+    // get a node object
     vnode = defineVnode(vnode);
+    // if the previous state exists, it obtains the state
+    let { vnode: vprevnode, handlers = {}, updateComponent } =
+        (prevNode && prevNode[ID]) || {};
+    // if the node stored in the previous state is identical to the current one,
+    // it will not execute the update process
+    if (vnode === vprevnode) return prevNode;
 
     let {
-            /**
-             * shows that keys are kept within children
-             * @example
-             * {1:true,3:true,5:true}
-             **
-             * these keys allow you to clean existing children in useKeys.             */
-            useKeys,
-            // shows if the vnode modifies the context
-            useContext,
-            // shows if the vdodo uses with shadowDom
-            useShadowDom,
-            useChildre,
-            // define the nodeName | component associated with the deer
-            tag: nextTag,
-            //  properties to define the node
-            props: nextProps,
-            // children to deliver to the node
-            children: nextChildren
-        } = vnode,
-        // define si el situiente tag es un timpo componente
-        isComponent = typeof nextTag === "function",
-        // is reserved to be occupied by the current component
-        component;
-    // define if the node tree is of type svg
-    isSvg = nextTag === "svg" || isSvg;
-    // create or maintain your current context
-    context = useContext ? { ...context, ...useContext } : context;
-    /**
-     * Host is only maintained until the continuity of high-order components
-     * is broken, from root render
-     */
-    if (!isComponent && !deep) {
-        isHost = false;
+        // defines the next node to manipulate the concurrent tree
+        tag: nextTag,
+        // define the properties that the next node must possess
+        props: nextProps,
+        // define the children that this node must possess in the following state
+        children: nextChildren,
+        // transmits keys to the updateChildren that is kept in the children list
+        useKeys,
+        // define if they would use updateChildren
+        useChildren,
+        // define if shadowDom was used
+        useShadowDom
+    } = vnode;
+    // define if the tree is of the SVG type
+    isSvg = isSvg || nextTag === "svg";
+
+    let nextNode = prevNode,
+        isFunction = typeof nextTag === "function";
+
+    // create an updateComponent
+    if (isFunction && !updateComponent) {
+        updateComponent = createUpdateComponent(ID, isSvg);
     }
 
-    let nameSpace = isHost ? STATE_HOST : STATE,
-        prevState = (prevNode && prevNode[nameSpace]) || {};
-
-    let { props: prevProps } = prevState.vnode || {};
-
-    // check if the previous state is identical to the current one, if so avoid the process
-    if (prevState.vnode === vnode) {
-        return prevNode;
+    if (
+        getNodeName(prevNode) !== nextTag &&
+        nextTag !== "host" &&
+        !isFunction
+    ) {
+        nextNode = createNode(nextTag, isSvg);
+        handlers = {};
+        if (prevNode) {
+            prevNode.parentNode.replaceChild(nextNode, prevNode);
+        }
     }
-
-    // obtains the list of components associated with the node
-    components = prevState.components || components;
-    // get the current component based on the depth of the list
-    component = components[deep];
-
-    /**
-     * Check if the current component is different requested
-     * if it is different, we proceed to clear this
-     */
-    if (component && component.tag !== nextTag) {
-        clearComponentEffects(components.splice(deep), true);
-        component = false;
-    }
-    /**
-     * recover or create the current component
-     */
-
-    if (isComponent) {
-        // check if the component already has an instance, if it does not own, create one.
-        if ((component || {}).tag !== nextTag) {
-            components[deep] = createComponent(
-                nextTag,
-                isHost,
+    if (updateComponent && currentUpdateComponent !== updateComponent) {
+        return updateComponent(COMPONENT_UPDATE, nextNode, vnode, context);
+    } else if (nextTag !== NODE_TEXT) {
+        updateProperties(nextNode, nextProps, handlers, isSvg);
+        if (useChildren && (vprevnode || {}).children !== vnode.children) {
+            updateChildren(
+                ID,
+                useShadowDom ? nextNode.shadowRoot || nextNode : nextNode,
+                nextChildren,
+                useKeys,
                 isSvg,
-                deep,
-                components
+                context
             );
         }
-        // resumes the instance of the current component
-        component = components[deep];
-        // associates the previous state of the tag with the current one or creates a text type.
-        nextTag = defineNodeTag(prevNode) || NODE_TEXT;
-    }
-    // create or keep the next node to work
-    let nextNode = updateElement(prevNode, nextTag, isSvg);
-    // if prevNode has a definition, it proceeds to the replacement of the node
-    if (prevNode && prevNode !== nextNode) {
-        if (!component) clearElement(isHost, prevNode);
-        let parent = prevNode.parentNode;
-        if (parent) parent.replaceChild(nextNode, prevNode);
-    }
-    // if it is a component, the update is issued
-    if (component) {
-        component.base = nextNode;
-        component.props = nextProps;
-        component.context = context;
-        // if the component maintains a wait state, its update is ignored
-        return component.prevent ? nextNode : component.render();
-    } else if (nextTag) {
-        // updates the properties associated with the node
-        updateProperties(nextNode, prevProps, nextProps, isHost, isSvg);
-        // update the children of the node
-
-        if (!useChildre)
-            updateChildren(
-                useShadowDom ? nextNode.shadowRoot || nextNode : nextNode,
-                nextChildren || [],
-                isHost,
-                isSvg,
-                context,
-                useKeys
-            );
+    } else {
+        if (nextNode.nodeValue !== nextChildren) {
+            nextNode.nodeValue = nextChildren;
+        }
     }
 
-    nextNode[nameSpace] = { vnode, components };
+    nextNode[ID] = { handlers, vnode, updateComponent };
 
     return nextNode;
 }
