cb31b9155b55d42826e2293857cef1c57cf2a9f0
Matias Trujillo <mtrujillo@outlook.cl>
2019-02-20 19:51:48 -0300

    setTask is added to the export

--- src/index.js
+++ src/index.js
@@ -1,44 +1,5 @@
-import { update } from "./update";
-import { vnode } from "./vnode";
+export { render } from "./update";
+export { h } from "./vnode";
 export { options } from "./options";
-
-export { useState, useEffect, useContext, useRef, useMemo } from "./hooks";
-
-export function h(tag, props, ...children) {
-    return vnode(tag, props, children);
-}
-
-export { createContext } from "./createContext";
-/**
- * Render translates the vnode to DOM state
- * @param {*} vnode
- * @param {*} node
- * @param {*} isHost - this utility is added with the purpose of
- *                     promoting the use of web-components, I have
- *                     modified the process of updating the DOM,
- *                     with the intention of facilitating the use of
- *                     the host tag when working with web-components,
- *                     this allows to create a parallel state that is
- *                     not shared with the default state.
- *
- *                     when enabling host use, you should always
- *                     return the <host /> tag
- */
-export function render(vnode, node, isHost) {
-    if (isHost) {
-        if (typeof vnode.tag !== "function") {
-            throw new Error(
-                "If host is defined as true, the vnode must be a component"
-            );
-        }
-    }
-    update(
-        node,
-        isHost
-            ? vnode
-            : typeof vnode === "object" && vnode.tag === "host"
-            ? vnode
-            : h("host", {}, vnode),
-        isHost
-    );
-}
+export { setTask } from "./task";
+export { useEffect, useState, useRef, useContext, useMemo } from "./hooks";
