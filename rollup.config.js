fcf363e2be4fd834c569f6c00589d97e5a67ea24
Matias Trujillo <mtrujillo@outlook.cl>
2019-02-22 16:51:40 -0300

    microbundle => rollup

---
+++ rollup.config.js
@@ -0,0 +1,32 @@
+import pkg from "./package.json";
+import buble from "rollup-plugin-buble";
+import { terser } from "rollup-plugin-terser";
+
+export default {
+    input: pkg.source,
+    output: [
+        {
+            file: pkg.main,
+            sourcemap: true,
+            format: "cjs"
+        },
+        {
+            file: pkg["umd:main"],
+            sourcemap: true,
+            format: "umd",
+            name: pkg.name
+        },
+        {
+            file: pkg["module"],
+            sourcemap: true,
+            format: "es"
+        }
+    ],
+    plugins: [
+        buble({
+            jsx: "h",
+            objectAssign: "Object.assign"
+        }),
+        terser()
+    ]
+};
