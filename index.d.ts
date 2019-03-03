eb3976437da73f145a48939b0bde7262f02fea59
uppercod <mtrujillo@outlook.cl>
2019-03-03 00:24:30 -0300

    updat pkg 1.2.4

---
+++ index.d.ts
@@ -0,0 +1,39 @@
+interface options{
+    document?:object,
+    maxConcurrentTask?:number,
+}
+
+interface Context{
+    Provider(props:object,context:object);
+    Consumer(props:object,context:object);
+}
+
+interface setState{
+    (nextState:any):void
+}
+
+interface ref{
+    current:any;
+}
+
+interface Action{
+    type : any,
+    payload?:any
+}
+
+interface dispatch{
+    (action:Action)
+}
+
+declare module "@atomico/core"{
+    export let options:options;
+    export function h(tag:string|Function,props?:object,...children:any):object;
+    export function render(vnode:object,node:HTMLElement|SVGElement|Text,disableHost?:boolean,customID?:string):void;
+    export function setTask(task:Function,argument?:any,lvl?:number):void;
+    export function createContext(value:any):Context;
+    export function useState(initialState:any):[any,setState];
+    export function useEffect(handler:Function,args?:any[]):void;
+    export function useMemo(handler:Function,args:any[]):any;
+    export function useReducer(reducer:Function,initialState:any):[any,dispatch];
+    export function useRef(current:any):ref;
+}
