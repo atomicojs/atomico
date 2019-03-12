8c01282e4fa838a5f11137529ea3cf9b132dcda2
uppercod <mtrujillo@outlook.cl>
2019-03-12 14:49:17 -0300

    the definition of constants is simplified

--- src/constants.js
+++ src/constants.js
@@ -1,17 +1,16 @@
 export const ARRAY_EMPTY = [];
 export const EVENT_ALIAS = {};
-
+export const OBJECT_EMPTY = {};
 export const CONTEXT = "@ctx.";
 
 export const STATE = "@state";
-export const NODE_NAME = "@type";
-export const NODE_HANDLERS = "@handlers";
+export const NODE_TYPE = "@type";
 
 export const NODE_HOST = "host";
 
-export const COMPONENT_CREATE = "@create";
-export const COMPONENT_UPDATE = "@update";
-export const COMPONENT_CREATED = "@created";
-export const COMPONENT_UPDATED = "@updated";
-export const COMPONENT_CLEAR = "@clear";
-export const COMPONENT_REMOVE = "@remove";
+export const COMPONENT_CREATE = 0x61;
+export const COMPONENT_UPDATE = 0x74;
+export const COMPONENT_CREATED = 0x4f;
+export const COMPONENT_UPDATED = 0x4d;
+export const COMPONENT_CLEAR = 0x49;
+export const COMPONENT_REMOVE = 0x43;
