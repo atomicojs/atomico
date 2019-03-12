170d7cc5cddbc7ebc58223e013bc7217ffd26506
uppercod <mtrujillo@outlook.cl>
2019-03-12 15:06:42 -0300

    rewrite

--- src/render.js
+++ src/render.js
@@ -1,12 +1,13 @@
 import { diff } from "./diff";
 import { toVnode, createElement } from "./vnode";
-import { STATE, NODE_HOST } from "./constants";
+import { STATE, NODE_HOST, OBJECT_EMPTY } from "./constants";
 
 export function render(vnode, parent, ID) {
     ID = ID || STATE;
+
     vnode = toVnode(vnode);
-    if (vnode.type != null && vnode.type != NODE_HOST) {
+    if (ID == STATE && vnode.type != NODE_HOST) {
         vnode = createElement(NODE_HOST, {}, vnode);
     }
-    diff(ID, parent, vnode);
+    diff(ID, parent, vnode, OBJECT_EMPTY);
 }
