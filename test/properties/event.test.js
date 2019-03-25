f437db1ce644d52ef288aa493e7a5915907e2488
uppercod <mtrujillo@outlook.cl>
2019-03-25 01:05:19 -0300

    Creation and event elimination test is added

---
+++ test/properties/event.test.js
@@ -0,0 +1,29 @@
+import { container } from "../util";
+import { h, render } from "../../dist/atomico";
+import { exportSpecifier, exportAllDeclaration } from "@babel/types";
+
+describe("Event", () => {
+	test("listener and unlistener event", () => {
+		let scope = container(),
+			ref = {},
+			call = 0;
+
+		function onClick() {
+			call++;
+		}
+
+		function dispatch() {
+			ref.current.dispatchEvent(new CustomEvent("click"));
+		}
+
+		render(<button ref={ref} onClick={onClick} />, scope);
+
+		dispatch();
+
+		render(<button ref={ref} />, scope);
+
+		dispatch();
+
+		expect(call).toBe(1);
+	});
+});
