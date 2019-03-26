ff8b7d49c9cd819aff8f01a116dac008bbf826f1
uppercod <mtrujillo@outlook.cl>
2019-03-26 00:24:17 -0300

    the export of toChildren => toList is modified

--- src/index.js
+++ src/index.js
@@ -1,12 +1,5 @@
-// export { render } from "./update";
-// export { h } from "./vnode";
-// export { options } from "./options";
-// export { setTask } from "./utils";
-// export { useEffect, useState, useRef, useMemo, useReducer } from "./hooks";
-// export { createContext, useContext } from "./createContext";
-
 import { createElement } from "./vnode";
-export { toChildren } from "./diff";
+export { toList } from "./diff";
 export { render } from "./render";
 
 export let h = createElement;
