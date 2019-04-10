5df05446c9305411f91d7889899ff8c14d7c0999
Uppercod <mtrujillo@outlook.cl>
2019-04-10 12:07:18 -0300

    solves the context update only when it is immutable

--- src/create-context.js
+++ src/create-context.js
@@ -4,34 +4,26 @@ import { getCurrentComponent } from "./component";
 
 let counter = 0;
 
-function Context(props) {
-    return props.children;
-}
-
-export function useContext(Context) {
-    let context = getCurrentComponent().context || {};
-    return Context in context ? context[Context] : Context.defaultValue;
+export function useContext({ id, defaultValue }) {
+	let context = getCurrentComponent().context || {};
+	return id in context ? context[id] : defaultValue;
 }
 
 export function createContext(defaultValue) {
-    let nameSpace = CONTEXT + counter++,
-        context = { Provider, Consumer, toString, defaultValue };
+	let id = CONTEXT + counter++,
+		Context = { Provider, Consumer, id, defaultValue };
 
-    function Provider({ value, children }) {
-        let snap = getCurrentComponent();
-        if (context[nameSpace] != value) {
-            snap.context = assign({}, snap.context, {
-                [nameSpace]: value
-            });
-        }
-
-        return children;
-    }
-    function Consumer({ children }) {
-        return children(useContext(context));
-    }
-    function toString() {
-        return nameSpace;
-    }
-    return context;
+	function Provider({ value, children }) {
+		let snap = getCurrentComponent();
+		if (snap.context[id] != value) {
+			snap.context = assign({}, snap.context, {
+				[id]: value
+			});
+		}
+		return children;
+	}
+	function Consumer({ children }) {
+		return children(useContext(Context));
+	}
+	return Context;
 }
