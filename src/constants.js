aeff2e861fd8bc1f405649f4a276e1234eec088d
Uppercod <mtrujillo@outlook.cl>
2019-04-10 21:19:31 -0300

    is modified const => let

--- src/constants.js
+++ src/constants.js
@@ -1,28 +1,28 @@
-export const ARRAY_EMPTY = [];
-export const EVENT_ALIAS = {};
-export const OBJECT_EMPTY = {};
-export const CONTEXT = "@ctx.";
+export let ARRAY_EMPTY = [];
+export let EVENT_ALIAS = {};
+export let OBJECT_EMPTY = {};
+export let CONTEXT = "@ctx.";
 
-export const STATE = "@state";
-export const NODE_TYPE = "@type";
+export let STATE = "@state";
+export let NODE_TYPE = "@type";
 
-export const NODE_HOST = "host";
+export let NODE_HOST = "host";
 
-export const COMPONENT_CREATE = 0x61;
-export const COMPONENT_UPDATE = 0x74;
-export const COMPONENT_CREATED = 0x4f;
-export const COMPONENT_UPDATED = 0x4d;
-export const COMPONENT_CLEAR = 0x49;
-export const COMPONENT_REMOVE = 0x43;
+export let COMPONENT_CREATE = 0x61;
+export let COMPONENT_UPDATE = 0x74;
+export let COMPONENT_CREATED = 0x4f;
+export let COMPONENT_UPDATED = 0x4d;
+export let COMPONENT_CLEAR = 0x49;
+export let COMPONENT_REMOVE = 0x43;
 
-export const IGNORE = {
+export let IGNORE = {
 	children: 1
 };
 
-export const IGNORE_CHILDREN = {
+export let IGNORE_CHILDREN = {
 	innerHTML: 1,
 	textContent: 1,
 	contenteditable: 1
 };
 
-export const CSS_PROPS = {};
+export let CSS_PROPS = {};
