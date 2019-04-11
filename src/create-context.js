5493c2b1ae6dfe679cdc27698bcd4a6e82b7e8d5
Uppercod <mtrujillo@outlook.cl>
2019-04-10 21:30:06 -0300

    resolve concurrent context reading bug

--- src/create-context.js
+++ src/create-context.js
@@ -1,27 +1,44 @@
 import { CONTEXT } from "./constants";
 import { assign } from "./utils";
 import { getCurrentComponent } from "./component";
-
+/**
+ * @typedef {import("./vnode").Vnode} Vnode
+ */
+/**@type {number} */
 let counter = 0;
-
+/**
+ * @param {{id:string,defaultValue:any}}
+ */
 export function useContext({ id, defaultValue }) {
-	let context = getCurrentComponent().context || {};
-	return id in context ? context[id] : defaultValue;
+	let component = getCurrentComponent().component;
+	return id in component.context ? component.context[id] : defaultValue;
 }
-
+/**
+ * create a context object, which manipulates the context of the tree
+ * @param {*} defaultValue
+ */
 export function createContext(defaultValue) {
 	let id = CONTEXT + counter++,
 		Context = { Provider, Consumer, id, defaultValue };
-
+	/**
+	 * create a new context for children
+	 * @param {{value:any,children:Vnode}}
+	 * @returns {Vnode}
+	 */
 	function Provider({ value, children }) {
-		let snap = getCurrentComponent();
-		if (snap.context[id] != value) {
-			snap.context = assign({}, snap.context, {
+		let { component } = getCurrentComponent();
+		if (component.context[id] != value) {
+			component.context = assign({}, component.context, {
 				[id]: value
 			});
 		}
 		return children;
 	}
+	/**
+	 * Get the current context
+	 * @param {{children:Function}}
+	 * @returns {Vnode}
+	 */
 	function Consumer({ children }) {
 		return children(useContext(Context));
 	}
