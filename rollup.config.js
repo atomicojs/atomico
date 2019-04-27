c00e363352f22c1ea4e7d5b1070f7a4ac4a8936d
Uppercod <mtrujillo@outlook.cl>
2019-04-26 23:21:12 -0400

    Adding options render, update pkg 1.4.0, now the pkg generates only 2 exports module.js and browser.js,

--- rollup.config.js
+++ rollup.config.js
@@ -1,39 +1,37 @@
 import pkg from "./package.json";
 import buble from "rollup-plugin-buble";
 import { terser } from "rollup-plugin-terser";
-import { sizeSnapshot } from "rollup-plugin-size-snapshot";
-export default {
-    input: pkg.source,
-    output: [
-        {
-            file: pkg.main,
-            sourcemap: true,
-            format: "cjs"
-        },
-        {
-            file: pkg["umd:main"],
-            sourcemap: true,
-            format: "umd",
-            name: pkg.name
-        },
-        {
-            file: pkg["module"],
-            sourcemap: true,
-            format: "es"
-        }
-    ],
-    plugins: [
-        buble({
-            jsx: "h",
-            objectAssign: "Object.assign"
-        }),
-        terser({
-            compress: {
-                keep_infinity: true,
-                pure_getters: true,
-                passes: 10
-            }
-        }),
-        sizeSnapshot()
-    ]
-};
+import size from "rollup-plugin-bundle-size";
+export default [
+	{
+		input: pkg.source,
+		output: {
+			file: pkg.module,
+			format: "es"
+		},
+		plugins: [size()]
+	},
+	{
+		input: pkg.source,
+		output: {
+			file: pkg.unpkg,
+			sourcemap: true,
+			format: "umd",
+			name: pkg.name
+		},
+		plugins: [
+			buble({
+				jsx: "h",
+				objectAssign: "Object.assign"
+			}),
+			terser({
+				compress: {
+					keep_infinity: true,
+					pure_getters: true,
+					passes: 10
+				}
+			}),
+			size()
+		]
+	}
+];
