2611cf75c6211f55a8a815efc7cf89fe371b25d6
Uppercod <mtrujillo@outlook.cl>
2019-05-15 17:34:30 -0400

    it simplifies and compresses the way in which userState is expressed, useReducer and useMemo

--- src/hooks.js
+++ src/hooks.js
@@ -1,6 +1,6 @@
 import { getCurrentComponent, useHook } from "./component.js";
 
-import { isEqualArray, assign, defer } from "./utils.js";
+import { isEqualArray, defer, isFunction } from "./utils.js";
 
 import {
 	COMPONENT_CREATE,
@@ -10,32 +10,27 @@ import {
 	COMPONENT_REMOVE
 } from "./constants.js";
 
+/**
+ * @return {HTMLElement}
+ */
 export function useHost() {
 	return getCurrentComponent().component.ref;
 }
 
 export function useState(initialState) {
-	let next = getCurrentComponent().next,
-		type = 0x9f;
-	let [state, dispatch] = useHook((state, action) => {
-		switch (action.type) {
-			case COMPONENT_CREATE:
-				return typeof initialState == "function"
-					? initialState()
-					: initialState;
-			case type:
-				let nextState = action.state;
-				return typeof nextState == "function" ? nextState(state) : nextState;
-		}
-		return state;
-	});
-	return [
-		state,
-		state => {
-			dispatch({ state, type });
-			next();
-		}
-	];
+	/**@type {RefUseState} */
+	let ref = useHook(0, [])[0];
+	if (!ref[1]) {
+		let next = getCurrentComponent().next;
+		ref.push(
+			isFunction(initialState) ? initialState() : initialState,
+			function setState(nextState) {
+				ref[0] = isFunction(nextState) ? nextState() : nextState;
+				next();
+			}
+		);
+	}
+	return ref;
 }
 
 export function useEffect(callback, args) {
@@ -51,7 +46,7 @@ export function useEffect(callback, args) {
 						(args && state.args ? !isEqualArray(args, state.args) : true);
 					if (next) state.clear.then(handler => handler && handler());
 				}
-				return assign({}, state, { args });
+				return { args };
 			case COMPONENT_CREATED:
 			case COMPONENT_UPDATED:
 				let next =
@@ -61,47 +56,43 @@ export function useEffect(callback, args) {
 				if (next) {
 					clear = defer(callback);
 				}
-				return assign({}, state, { clear, args });
+				return { clear, args };
 		}
 		return state;
 	});
 }
 
 export function useRef(current) {
-	let [ref] = useHook(false, { current });
-	return ref;
+	return useHook(0, { current })[0];
 }
 
 export function useMemo(callback, args) {
-	let [ref] = useHook(false, {});
-	if (!ref.args) {
-		ref.value = callback();
-	} else {
-		if (!isEqualArray(ref.args, args)) {
-			ref.value = callback();
-		}
+	let ref = useHook(0, [])[0];
+	if (!ref[0] || (ref[0] && !isEqualArray(ref[0], args))) {
+		ref[1] = callback();
 	}
-	ref.args = args;
-	return ref.value;
+	ref[0] = args;
+	return ref[1];
 }
 
 export function useReducer(reducer, initialState) {
-	let next = getCurrentComponent().next,
-		type = 0x9e;
-	let [state, dispatch] = useHook((state, action) => {
-		switch (action.type) {
-			case COMPONENT_CREATE:
-				return initialState;
-			case type:
-				return reducer(state, action.use);
-		}
-		return state;
-	});
-	return [
-		state,
-		use => {
-			dispatch({ type, use });
+	/** @type {RefUseReducer} */
+	let ref = useHook(0, [])[0];
+	if (!ref.current) {
+		let next = getCurrentComponent().next;
+		ref.push(initialState, (state, action) => {
+			ref[0] = ref[2](state, action);
 			next();
-		}
-	];
+		});
+	}
+	ref[2] = reducer;
+	return ref;
 }
+
+/**
+ * @typedef {function(*,*):void} SetState - **setState( nextState )**
+ *
+ * @typedef {[*, SetState]} RefUseState - **[state, setState]**
+ *
+ * @typedef {[*, import("./component").Dispatch]} RefUseReducer - **[state, dispatch]**
+ **/
