1fc1eb86a93fe3b1cde0f905e2fa6e508e3efa72
Uppercod <mtrujillo@outlook.cl>
2019-05-14 22:53:20 -0400

    the variables are defined for a better quality of use

--- src/constants.js
+++ src/constants.js
@@ -1,8 +1,7 @@
 export const ARRAY_EMPTY = [];
-export const EVENT_ALIAS = {};
 export const OBJECT_EMPTY = {};
 
-export const STATE = "@state";
+export const STATE = Symbol("Atomico.state");
 export const NODE_TYPE = "localName";
 
 export const NODE_HOST = "host";
@@ -14,7 +13,7 @@ export const COMPONENT_UPDATED = 0x4d;
 export const COMPONENT_CLEAR = 0x49;
 export const COMPONENT_REMOVE = 0x43;
 
-export const IGNORE = {
+export const IGNORE_PROPS = {
 	children: 1
 };
 
@@ -24,7 +23,7 @@ export const IGNORE_CHILDREN = {
 	contenteditable: 1
 };
 
-export const FROM_DOM_PROPS = {
+export const HYDRATE_PROPS = {
 	className: 1,
 	id: 1,
 	checked: 1,
@@ -32,4 +31,5 @@ export const FROM_DOM_PROPS = {
 	selected: 1
 };
 
-export const CSS_PROPS = {};
+export const MEMO_EVENT_NAME = {};
+export const MEMO_CSS_PROPS = {};
