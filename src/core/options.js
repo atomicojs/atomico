d8e312388fbd1b5b1a41f04dfca7373de2d51fa0
UpperCod <mtrujillo@outlook.cl>
2019-07-14 19:06:10 -0400

    now the hydration of the shadowDom depends on a css selector, this by default is [data-shadow-dom], but it can be customized using options.hydrationQueryShadowDom, the vnode proxy option is added

--- src/core/options.js
+++ src/core/options.js
@@ -1,1 +1,6 @@
-export let options = {};
+export let options = {
+	/**
+	 * allows defining a custom query selector valid for the hydration of shadowDom
+	 */
+	hydrationQueryShadowDom: "[data-shadow-dom]"
+};
