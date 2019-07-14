caf03ac91f2fcddfdfabde61d60f2c9af92d268d
UpperCod <mtrujillo@outlook.cl>
2019-07-14 19:07:16 -0400

    options are added to the export

--- src/core/index.js
+++ src/core/index.js
@@ -1,4 +1,5 @@
 export { createElement, createElement as h } from "./vnode";
 export { toList } from "./diff";
 export { render } from "./render";
+export { options } from "./options";
 export * from "./hooks";
