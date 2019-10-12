aad782ec993800cca3782d45cc76ef22e2775ffe
UpperCod <mtrujillo@outlook.cl>
2019-10-12 00:12:48 -0300

    add test

---
+++ src/tests/render/element.test.js
@@ -0,0 +1,119 @@
+import { h } from "../../core/core";
+import { customElementScope } from "../utils";
+
+describe("properties", () => {
+	it("transfer of prop to virtual-dom", async () => {
+		let value = "10";
+
+		function Wc({ value }) {
+			return <host>{value}</host>;
+		}
+
+		Wc.props = {
+			value: Number
+		};
+
+		let node = customElementScope(Wc);
+
+		document.body.appendChild(node);
+
+		node.value = value;
+
+		await node.rendered;
+
+		expect(node.textContent).toBe(value);
+
+		node.value = value = value + value;
+
+		await node.rendered;
+
+		expect(node.textContent).toBe(value);
+	});
+	it("property definition from the host tag", async () => {
+		let cn = "my-class";
+
+		function Wc({ cn }) {
+			return <host class={cn}></host>;
+		}
+
+		Wc.props = {
+			cn: String
+		};
+
+		let node = customElementScope(Wc);
+
+		document.body.appendChild(node);
+
+		node.cn = cn;
+
+		await node.rendered;
+
+		expect(node.className).toBe(cn);
+	});
+	it("schema Number", async () => {
+		function Wc() {
+			return "";
+		}
+
+		Wc.props = {
+			value: Number
+		};
+
+		let node = customElementScope(Wc);
+
+		document.body.appendChild(node);
+
+		await node.rendered;
+
+		node.setAttribute("value", "1000");
+
+		await node.rendered;
+
+		expect(node.value).toBe(1000);
+	});
+	it("schema Object", async () => {
+		function Wc() {
+			return "";
+		}
+
+		Wc.props = {
+			value: Object
+		};
+
+		let node = customElementScope(Wc);
+		let value = { value: 10 };
+
+		document.body.appendChild(node);
+
+		await node.rendered;
+
+		node.setAttribute("value", JSON.stringify(value));
+
+		await node.rendered;
+
+		expect(node.value).toEqual(value);
+	});
+
+	it("schema Array", async () => {
+		function Wc() {
+			return "";
+		}
+
+		Wc.props = {
+			value: Array
+		};
+
+		let node = customElementScope(Wc);
+		let value = [{ value: 10 }];
+
+		document.body.appendChild(node);
+
+		await node.rendered;
+
+		node.setAttribute("value", JSON.stringify(value));
+
+		await node.rendered;
+
+		expect(node.value).toEqual(value);
+	});
+});
---
+++ src/tests/render/set-attribute.test.js
@@ -0,0 +1,30 @@
+import { h, render } from "../../core/core";
+
+describe("set-attribute", () => {
+	it("basic", async () => {
+		let node = document.createElement("div");
+
+		let attrs = {
+			class: "className",
+			id: "id",
+			"data-set": "data-set",
+			"any-attr": "any-attr"
+		};
+
+		render(<host {...attrs}></host>, node);
+
+		for (let key in attrs) {
+			expect(node.getAttribute(key)).toBe(attrs[key]);
+		}
+	});
+
+	it("set attribute object", async () => {
+		let node = document.createElement("div");
+
+		let example = { name: "any" };
+
+		render(<host example={example}></host>, node);
+
+		expect(node.getAttribute("example")).toBe(JSON.stringify(example));
+	});
+});
