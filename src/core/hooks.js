fd04ae6d17654f095d80ed7d10956a98ffe91196
UpperCod <mtrujillo@outlook.cl>
2019-11-12 01:19:48 -0300

    fix the comparison of the second useEffect argument

--- src/core/hooks.js
+++ src/core/hooks.js
@@ -1,156 +1,158 @@
 import {
-	HOOK_MOUNT,
-	HOOK_UPDATE,
-	HOOK_CURRENT,
-	HOOK_MOUNTED,
-	HOOK_UPDATED,
-	HOOK_UNMOUNT,
-	ARRAY_EMPTY
-} from "./constants";
-import { isFunction, isEqualArray } from "./utils";
+    HOOK_MOUNT,
+    HOOK_UPDATE,
+    HOOK_CURRENT,
+    HOOK_MOUNTED,
+    HOOK_UPDATED,
+    HOOK_UNMOUNT,
+    ARRAY_EMPTY
+} from './constants';
+import { isFunction, isEqualArray } from './utils';
 
 function update(hook, type) {
-	hook[0] && (hook[1] = hook[0](hook[1], type));
+    hook[0] && (hook[1] = hook[0](hook[1], type));
 }
 
 function updateAll(hooks, type) {
-	for (let i in hooks) update(hooks[i], type);
+    for (let i in hooks) update(hooks[i], type);
 }
 
 export function useHook(reducer, initialState) {
-	if (HOOK_CURRENT.ref.hook) {
-		return HOOK_CURRENT.ref.hook.use(reducer, initialState)[1];
-	}
+    if (HOOK_CURRENT.ref.hook) {
+        return HOOK_CURRENT.ref.hook.use(reducer, initialState)[1];
+    }
 }
 
 export function useRender() {
-	return HOOK_CURRENT.ref.render;
+    return HOOK_CURRENT.ref.render;
 }
 
 export function useHost() {
-	return useHook(0, { current: HOOK_CURRENT.ref.host });
+    return useHook(0, { current: HOOK_CURRENT.ref.host });
 }
 
 export function createHookCollection(render, host) {
-	let hooks = {};
-	let mounted;
-	let hook = {
-		use,
-		load,
-		updated,
-		unmount
-	};
+    let hooks = {};
+    let mounted;
+    let hook = {
+        use,
+        load,
+        updated,
+        unmount
+    };
 
-	let ref = { hook, host, render };
+    let ref = { hook, host, render };
 
-	function load(callback, param) {
-		HOOK_CURRENT.index = 0;
-		HOOK_CURRENT.ref = ref;
-		let resolve = callback(param);
-		HOOK_CURRENT.ref = 0;
-		return resolve;
-	}
-	function use(reducer, state) {
-		let index = HOOK_CURRENT.index++;
-		let mount;
-		if (!hooks[index]) {
-			hooks[index] = [reducer, state];
-			mount = 1;
-		}
-		update(hooks[index], mount ? HOOK_MOUNT : HOOK_UPDATE);
-		return hooks[index];
-	}
-	function updated() {
-		updateAll(hooks, mounted ? HOOK_UPDATED : HOOK_MOUNTED);
-		mounted = 1;
-	}
-	function unmount() {
-		updateAll(hooks, HOOK_UNMOUNT);
-	}
-	return hook;
+    function load(callback, param) {
+        HOOK_CURRENT.index = 0;
+        HOOK_CURRENT.ref = ref;
+        let resolve = callback(param);
+        HOOK_CURRENT.ref = 0;
+        return resolve;
+    }
+    function use(reducer, state) {
+        let index = HOOK_CURRENT.index++;
+        let mount;
+        // record the hook and the initial state of this
+        if (!hooks[index]) {
+            hooks[index] = [null, state];
+            mount = 1;
+        }
+        // The hook always receives the last reduce.
+        hooks[index][0] = reducer;
+        update(hooks[index], mount ? HOOK_MOUNT : HOOK_UPDATE);
+        return hooks[index];
+    }
+    function updated() {
+        updateAll(hooks, mounted ? HOOK_UPDATED : HOOK_MOUNTED);
+        mounted = 1;
+    }
+    function unmount() {
+        updateAll(hooks, HOOK_UNMOUNT);
+    }
+    return hook;
 }
 
 export function useState(initialState) {
-	let render = useRender();
-	return useHook((state, type) => {
-		if (HOOK_MOUNT == type) {
-			state[0] = isFunction(initialState) ? initialState() : initialState;
-			state[1] = nextState => {
-				state[0] = isFunction(nextState)
-					? nextState(state[0])
-					: nextState;
-				render();
-			};
-		}
-		return state;
-	}, []);
+    let render = useRender();
+    return useHook((state, type) => {
+        if (HOOK_MOUNT == type) {
+            state[0] = isFunction(initialState) ? initialState() : initialState;
+            state[1] = nextState => {
+                state[0] = isFunction(nextState)
+                    ? nextState(state[0])
+                    : nextState;
+                render();
+            };
+        }
+        return state;
+    }, []);
 }
 
 export function useEffect(callback, args) {
-	let state = useHook(
-		(state, type) => {
-			switch (type) {
-				case HOOK_UPDATE:
-				case HOOK_UNMOUNT:
-					if (state[1]) {
-						let next =
-							type == HOOK_UNMOUNT ||
-							(args && state[0]
-								? !isEqualArray(args, state[0])
-								: true);
-						if (next) {
-							state[1]();
-							state[1] = 0;
-						}
-					}
-					break;
-				case HOOK_MOUNTED:
-				case HOOK_UPDATED:
-					let next =
-						type == HOOK_MOUNTED ||
-						(args && state[0]
-							? !isEqualArray(args, state[0])
-							: true);
-					if (next) {
-						state[1] = state[2]();
-					}
-					break;
-			}
-			return state;
-		},
-		[args]
-	);
-	state[2] = callback;
+    let state = useHook((state, type) => {
+        switch (type) {
+            case HOOK_UPDATE:
+            case HOOK_UNMOUNT:
+                if (state[1]) {
+                    // compare the previous snapshot with the generated state
+                    let next =
+                        type == HOOK_UNMOUNT ||
+                        (args && state[0]
+                            ? !isEqualArray(args, state[0])
+                            : true);
+                    if (next) {
+                        state[1]();
+                        // clean the effect collector
+                        state[1] = 0;
+                    }
+                }
+                break;
+            case HOOK_MOUNTED:
+            case HOOK_UPDATED:
+                let next =
+                    type == HOOK_MOUNTED ||
+                    (args && state[0] ? !isEqualArray(args, state[0]) : true);
+                if (next) {
+                    // save the effect collector
+                    state[1] = callback();
+                }
+                // save the comparison argument
+                state[0] = args;
+                break;
+        }
+        return state;
+    }, []);
 }
 
 export function useRef(current) {
-	return useHook(0, { current });
+    return useHook(0, { current });
 }
 
 export function useMemo(callback, args = ARRAY_EMPTY) {
-	let state = useHook(0, []);
+    let state = useHook(0, []);
 
-	if (!state[0] || (state[0] && !isEqualArray(state[0], args))) {
-		state[1] = callback();
-	}
-	state[0] = args;
-	return state[1];
+    if (!state[0] || (state[0] && !isEqualArray(state[0], args))) {
+        state[1] = callback();
+    }
+    state[0] = args;
+    return state[1];
 }
 
 export function useReducer(reducer, initialState) {
-	let render = useRender();
-	let hook = useHook((state, type) => {
-		if (HOOK_MOUNT == type) {
-			state[0] = initialState;
-			state[1] = action => {
-				state[0] = state[2](state[0], action);
-				render();
-			};
-		}
-		return state;
-	}, []);
+    let render = useRender();
+    let hook = useHook((state, type) => {
+        if (HOOK_MOUNT == type) {
+            state[0] = initialState;
+            state[1] = action => {
+                state[0] = state[2](state[0], action);
+                render();
+            };
+        }
+        return state;
+    }, []);
+    // allows the reduce to always access the scope of the component
+    hook[2] = reducer;
 
-	hook[2] = reducer;
-
-	return hook[1];
+    return hook[1];
 }
--- src/core/vnode.js
+++ src/core/vnode.js
@@ -1,9 +1,9 @@
 import {
-	SUPPORT_STYLE_SHEET,
-	META_STYLE_SHEET,
-	META_MAP_CHILDREN,
-	STYLE_SHEET_KEY,
-	META_KEYES
+    SUPPORT_STYLE_SHEET,
+    META_STYLE_SHEET,
+    META_MAP_CHILDREN,
+    STYLE_SHEET_KEY,
+    META_KEYES
 } from "./constants";
 
 import { isArray, isFunction } from "./utils";
@@ -17,94 +17,94 @@ let vNodeEmpty = createElement(null, { children: "" });
  * @returns {Vnode}
  **/
 export function createElement(nodeType, props, ...children) {
-	let vnode = { children, ...props, nodeType: nodeType || null };
-	return vnode;
+    let vnode = { children, ...props, nodeType: nodeType || null };
+    return vnode;
 }
 /**
  * toVnode, processes the object for correct use within the diff process.
  **/
 export function toVnode(value) {
-	if (isVnodeValue(value)) {
-		return value;
-	} else {
-		if (!value[META_MAP_CHILDREN]) {
-			let scan = mapChildren(value.children);
-			value.children = scan.children;
-			if (scan.keyes) {
-				value[META_KEYES] = scan.keyes;
-			}
-			value[META_MAP_CHILDREN] = true;
-		}
-		if (value.styleSheet && !SUPPORT_STYLE_SHEET) {
-			if (!value[META_STYLE_SHEET]) {
-				value.children.unshift(
-					toVnode(
-						createElement(
-							"style",
-							value[META_KEYES] ? { key: STYLE_SHEET_KEY } : {},
-							value.styleSheet
-						)
-					)
-				);
-				if (value[META_KEYES]) {
-					value[META_KEYES].unshift(STYLE_SHEET_KEY);
-				}
-			}
-			value[META_STYLE_SHEET] = true;
-		}
-	}
-	return value;
+    if (isVnodeValue(value)) {
+        return value;
+    } else {
+        if (!value[META_MAP_CHILDREN]) {
+            let scan = mapChildren(value.children);
+            value.children = scan.children;
+            if (scan.keyes) {
+                value[META_KEYES] = scan.keyes;
+            }
+            value[META_MAP_CHILDREN] = true;
+        }
+        if (value.styleSheet && !SUPPORT_STYLE_SHEET) {
+            if (!value[META_STYLE_SHEET]) {
+                value.children.unshift(
+                    toVnode(
+                        createElement(
+                            "style",
+                            value[META_KEYES] ? { key: STYLE_SHEET_KEY } : {},
+                            value.styleSheet
+                        )
+                    )
+                );
+                if (value[META_KEYES]) {
+                    value[META_KEYES].unshift(STYLE_SHEET_KEY);
+                }
+            }
+            value[META_STYLE_SHEET] = true;
+        }
+    }
+    return value;
 }
 
 function mapChildren(children, scan = { children: [] }, deep = 0) {
-	if (isArray(children)) {
-		let length = children.length;
-		for (let i = 0; i < length; i++) {
-			mapChildren(children[i], scan, deep + 1);
-		}
-	} else {
-		if (children == null && !deep) return scan;
+    if (isArray(children)) {
+        let length = children.length;
+        for (let i = 0; i < length; i++) {
+            mapChildren(children[i], scan, deep + 1);
+        }
+    } else {
+        if (children == null && !deep) return scan;
 
-		let vnode = toVnode(children);
+        let vnode = toVnode(children);
 
-		if (vnode != null && typeof vnode == "object") {
-			if (isFunction(vnode.nodeType)) {
-				let { nodeType, ...props } = vnode;
-				return mapChildren(nodeType(props), scan, deep + 1);
-			}
-			if ("key" in vnode) {
-				scan.keyes = scan.keyes || [];
-				if (!~scan.keyes.indexOf(vnode.key)) {
-					scan.keyes.push(vnode.key);
-				}
-			}
-		}
+        if (vnode != null && typeof vnode == "object") {
+            if (isFunction(vnode.nodeType)) {
+                let { nodeType, ...props } = vnode;
+                return mapChildren(nodeType(props), scan, deep + 1);
+            }
+            if ("key" in vnode) {
+                scan.keyes = scan.keyes || [];
+                if (!~scan.keyes.indexOf(vnode.key)) {
+                    scan.keyes.push(vnode.key);
+                }
+            }
+        }
 
-		scan.children.push(vnode);
-	}
-	return scan;
+        scan.children.push(vnode);
+    }
+    return scan;
 }
 
 export function isVnodeEmpty(value) {
-	let type = typeof value;
-	return value == null || type == "boolean" || type == "function";
+    let type = typeof value;
+    return value == null || type == "boolean" || type == "function";
 }
 
 export function fillVnodeValue(value) {
-	return isVnodeEmpty(value)
-		? vNodeEmpty
-		: createElement(null, { children: "" + value });
+    return isVnodeEmpty(value)
+        ? vNodeEmpty
+        : createElement(null, { children: "" + value });
 }
 
 export function isVnodeValue(value) {
-	let type = typeof value;
-	return (
-		value == null ||
-		type == "string" ||
-		type == "number" ||
-		type == "function" ||
-		type == "boolean"
-	);
+    let type = typeof value;
+    return (
+        value == null ||
+        type == "string" ||
+        type == "number" ||
+        type == "function" ||
+        type == "boolean"
+    );
 }
 
 /**
