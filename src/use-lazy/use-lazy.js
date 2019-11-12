9cad8572dd927e2e626940dab07984e7e788037d
UpperCod <mtrujillo@outlook.cl>
2019-11-12 01:20:13 -0300

    improves the useLazy hook behavior

--- src/use-lazy/use-lazy.js
+++ src/use-lazy/use-lazy.js
@@ -1,19 +1,32 @@
-import { useState } from "../core/core";
-import { fps } from "../core/utils";
+import { useState, useEffect } from '../core/core';
+import { fps, isFunction } from '../core/utils';
+
+let Loading = ({ loading, ...props }) => loading;
+
+let def = 'default';
+
 /**
  * It allows to load a component asynchronously.
  * @param {Function} callback
  * @param {object} [props]
  */
-export function useLazy(callback) {
-	let [view, setView] = useState(() => {
-		let ready;
-		let def = "default";
-		callback().then(
-			data =>
-				(ready = 1) && setView(() => (def in data ? data[def] : data))
-		);
-		fps(() => !ready && setView(() => ({ loading }) => loading));
-	});
-	return view;
+export function useLazy(callback, args = []) {
+    let [view, setView] = useState(() => Loading);
+    useEffect(() => {
+        let cancel;
+        let ready;
+
+        callback().then(data => {
+            ready = true;
+            if (!cancel) {
+                setView(() => (def in data ? data[def] : data));
+            }
+        });
+        fps(() => !ready && !cancel);
+        return () => {
+            cancel = true;
+            if (ready) setView(() => Loading);
+        };
+    }, args);
+    return view;
 }
