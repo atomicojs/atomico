e51dbb4774e2b58c8f18bccfa1508b5db654fc70
UpperCod <mtrujillo@outlook.cl>
2019-11-13 18:56:59 -0300

    add the usePublic hook

--- src/core/element/hooks.js
+++ src/core/element/hooks.js
@@ -2,25 +2,33 @@ import { useHost } from "../hooks";
 import { dispatchEvent } from "./utils";
 
 export function useProp(name) {
-	let ref = useHost();
-	if (name in ref.current) {
-		if (!ref[name]) {
-			ref[name] = [null, nextValue => (ref.current[name] = nextValue)];
-		}
-		ref[name][0] = ref.current[name];
-		return ref[name];
-	}
+    let ref = useHost();
+    if (name in ref.current) {
+        if (!ref[name]) {
+            ref[name] = [null, nextValue => (ref.current[name] = nextValue)];
+        }
+        ref[name][0] = ref.current[name];
+        return ref[name];
+    }
 }
 
 export function useEvent(type, customEventInit) {
-	let ref = useHost();
-	if (!ref[type]) {
-		ref[type] = detail =>
-			dispatchEvent(
-				ref.current,
-				type,
-				detail ? { ...customEventInit, detail } : customEventInit
-			);
-	}
-	return ref[type];
+    let ref = useHost();
+    if (!ref[type]) {
+        ref[type] = detail =>
+            dispatchEvent(
+                ref.current,
+                type,
+                detail ? { ...customEventInit, detail } : customEventInit
+            );
+    }
+    return ref[type];
+}
+
+export function usePublic(name, value) {
+    let { current } = useHost();
+    if (current[name] != value) {
+        current[name] = value;
+    }
+    return current[name];
 }
