fb64b22df8f8d1bf5cc0b034e39f120da52c4f85
UpperCod <mtrujillo@outlook.cl>
2019-11-14 01:54:26 -0300

    update pkg 0.13.3

--- rollup.config.js
+++ rollup.config.js
@@ -1,31 +1,30 @@
 import sizes from "@atomico/rollup-plugin-sizes";
 import { terser } from "rollup-plugin-terser";
 import resolve from "rollup-plugin-node-resolve";
-import pkg from "./package.json";
 
 export default {
-	input: [
-		"src/core/core.js",
-		"src/html.js",
-		"src/use-lazy/use-lazy.js",
-		"src/use-router/use-router.js",
-		"src/use-state-generator/use-state-generator.js"
-	],
-	output: {
-		dir: "./",
-		format: "es",
-		chunkFileNames: "chunk/[name].js",
-		sourcemap: true
-	},
-	plugins: [
-		resolve(),
-		...(process.env.ROLLUP_WATCH
-			? []
-			: [
-					terser({
-						sourcemap: true
-					}),
-					sizes()
-			  ])
-	]
+    input: [
+        "src/core/core.js",
+        "src/html.js",
+        "src/use-lazy/use-lazy.js",
+        "src/use-router/use-router.js",
+        "src/use-state-generator/use-state-generator.js"
+    ],
+    output: {
+        dir: "./",
+        format: "es",
+        chunkFileNames: "chunk/[name].js",
+        sourcemap: true
+    },
+    plugins: [
+        resolve(),
+        ...(process.env.ROLLUP_WATCH
+            ? []
+            : [
+                  //terser({
+                  //	sourcemap: true
+                  //}),
+                  sizes()
+              ])
+    ]
 };
