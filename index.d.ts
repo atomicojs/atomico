293244d53d526eec33b51aacbe45c86ce269ad76
UpperCod <mtrujillo@outlook.cl>
2020-01-24 12:57:44 -0300

    improve core type definition

--- index.d.ts
+++ index.d.ts
@@ -1,90 +1,122 @@
-type CurrentState = any;
+type CallbackReducer<T = any, S = any> = (state: T, action: S) => T;
 
-type NodeType = string | null | Function;
+type CallbackReducerState<
+    R extends CallbackReducer
+> = R extends CallbackReducer<infer T> ? T : never;
 
-type Action = { [index: string]: any; type?: any };
+type CallbackSetState<T> = (value: T | (() => T)) => void;
 
-type Ref = { [index: string]: any };
+type CallbackEffect = () => void | (() => void);
 
-type Dispatch = (action: Action) => null;
+type CallbackDispatch = (value: any) => void;
 
-type Reducer = (state: any, action: Action) => any;
+type ArgumentList = ReadonlyArray<any>;
 
-export type Types = Number | String | Boolean | Object;
-
-export type SchemaTypes = {
-    type: Types;
-    value: any;
-    reflect?: boolean;
-    event:
-        | boolean
-        | {
-              type?: string;
-              bubbles?: boolean;
-              detail?: any;
-              cancelable?: Boolean;
-          };
-};
-
-export interface Component {
-    (props: Object): any;
-    props: {
-        [index: string]: Types | SchemaTypes;
-    };
-    error?: Function;
+interface MutableRef<T> {
+    [index: string]: any;
+    current: T;
 }
+
+type PropTypes =
+    | NumberConstructor
+    | StringConstructor
+    | BooleanConstructor
+    | ObjectConstructor
+    | DateConstructor
+    | PromiseConstructor
+    | SymbolConstructor;
 
 declare module "atomico" {
-    export function customElement(
-        tagName: string | Component,
-        component?: Component
-    ): Function | HTMLElement;
-    export function h(
-        nodeType: NodeType,
-        props?: Object,
-        ...children: any[]
-    ): Object;
-    export function useState(initialState: any): [CurrentState, Function];
-    export function useEffect(callbackEffect: Function, vars?: any[]): void;
-    export function useMemo(callbackMemo: Function, vars?: any[]): any;
-    export function useCallback(callback: Function, vars?: any[]): Function;
-    export function useReducer(
-        callback: Reducer,
-        initialState?: any
-    ): [CurrentState, Dispatch];
-    export function useRef(current?: any): Ref;
-    export function useProp(observable: string): [CurrentState, Function];
+    /**
+     * interface for the declaration of a custom event
+     * ```tsx
+     * let myEvent:EventInit = {bubbles:true, detail : "...!"}
+     * ```
+     */
+    export interface EventInit {
+        type?: any;
+        detail?: any;
+        bubbles?: boolean;
+        cancelable?: boolean;
+        composed?: boolean;
+    }
+    /**
+     * interface to declare Objects as PropSchema, eg
+     * ```ts
+     * let myProp:PropSchema<number> = {
+     *    type : Number,
+     *    options : [1,2,10],
+     *    value:10
+     * }
+     * ```
+     */
+    export interface PropSchema<T = any> {
+        type: PropTypes;
+        options?: T[];
+        value?: T | (() => T);
+        reflect?: boolean;
+        event?: boolean | EventInit;
+    }
+    /**
+     * interface for the declaration of a component
+     * ```tsx
+     * const MyComponent:Component = (props)=><host/>;
+     * ```
+     */
+    export interface Component {
+        (props: { [index: string]: any }): any;
+        props?: {
+            [index: string]: PropTypes | PropSchema;
+        };
+        error?: Function;
+    }
+
+    export function useProp<T>(index: string): [T, CallbackSetState<T>];
+    /**
+     * Create a local state in the web-component
+     */
+    export function useState<T>(state: T): [T, CallbackSetState<T>];
+    /**
+     * Allows you to create side effects, useful for controlling effects that
+     * interactive with the DOM or asynchrony
+     */
+    export function useEffect(callback: CallbackEffect): void;
+    /**
+     * Create a reference
+     */
+    export function useRef<T>(current?: T): MutableRef<T>;
+    /**
+     * Allows access to the component, without the need to declare the reference
+     */
+    export function useHost(): MutableRef<HTMLElement>;
+    /**
+     * Memorize the return of a callback by limiting its execution through an array of arguments,
+     * the callback is executed at the time of rendering only if the arguments change
+     */
+    export function useMemo<T>(callback: () => T, args: ArgumentList): T;
+    /**
+     * Allows memorizing the callback based on the second parameter, has an effect similar to useMemo
+     */
+    export function useCallback<T extends (...args: any[]) => any>(
+        callback: T,
+        args: ArgumentList
+    ): T;
+    /**
+     * An alternative to useState. Accept a reduce and return the current state paired with a dispatch method.
+     */
+    export function useReducer<R extends CallbackReducer, S>(
+        reducer: R,
+        initialState?: S | CallbackReducerState<R>
+    ): [CallbackReducerState<R>, CallbackDispatch];
+    /**
+     * Allows a variable or function to be visible from the component
+     */
+    export function usePublic<T>(name: string, value: T): T;
+    /**
+     * Returns a callback that dispatches an event from the component
+     */
     export function useEvent(
-        type: string,
-        customEventInit?: {
-            detail?: any;
-            bubbles?: Boolean;
-            cancelable?: Boolean;
-            composed?: Boolean;
-        }
-    ): Function;
+        name: string,
+        config?: EventInit
+    ): (event: Event | CustomEvent) => void;
 }
-/**  
- * @todo  find a solution for the local sub modules of the package
-declare module "atomico/use-lazy" {
-    export function useLazy(callback: Function): Function;
-}
-
-declare module "atomico/use-router" {
-    export function useRedirect(path?: string): Function;
-    export function useRouter(router: { [index: string]: Function }): Function;
-    export function useRoute(path: string): [boolean, Object];
-}
-
-declare module "atomico/html" {
-    export default function html(template: string, ...values: any[]): Object;
-}
-
-declare module "atomico/use-state-generator" {
-    export function useStateGenerator(
-        callback: Function,
-        initialState: any,
-        vars: any[]
-    ): [any, Promise<any>];
-}
-*/
