ae542ca182c1b66607bd25672f6346f201f79ef2
UpperCod <mtrujillo@outlook.cl>
2020-02-18 03:06:57 -0300

    the base code of the hooks is isolated

--- src/core/hooks.js
+++ src/core/hooks.js
@@ -4,73 +4,14 @@ import {
     HOOK_CURRENT,
     HOOK_MOUNTED,
     HOOK_UPDATED,
-    HOOK_UNMOUNT
-} from "./constants";
-import { isFunction, isEqualArray } from "./utils";
-
-function update(hook, type) {
-    hook[0] && (hook[1] = hook[0](hook[1], type));
-}
-
-function updateAll(hooks, type) {
-    for (let i in hooks) update(hooks[i], type);
-}
-
-export function useHook(reducer, initialState) {
-    if (HOOK_CURRENT.ref.hook) {
-        return HOOK_CURRENT.ref.hook.use(reducer, initialState)[1];
-    }
-}
-
-export function useRender() {
-    return HOOK_CURRENT.ref.render;
-}
+    HOOK_UNMOUNT,
+    useHook,
+    useRender
+} from "./create-hooks";
+import { isEqualArray, isFunction } from "./utils";
 
 export function useHost() {
     return useHook(0, { current: HOOK_CURRENT.ref.host });
-}
-
-export function createHookCollection(render, host) {
-    let hooks = {};
-    let mounted;
-    let hook = {
-        use,
-        load,
-        updated,
-        unmount
-    };
-
-    let ref = { hook, host, render };
-
-    function load(callback, param) {
-        HOOK_CURRENT.index = 0;
-        HOOK_CURRENT.ref = ref;
-        let resolve = callback(param);
-        HOOK_CURRENT.ref = 0;
-        return resolve;
-    }
-    function use(reducer, state) {
-        let index = HOOK_CURRENT.index++;
-        let mount;
-        // record the hook and the initial state of this
-        if (!hooks[index]) {
-            hooks[index] = [null, state];
-            mount = 1;
-        }
-        // The hook always receives the last reduce.
-        hooks[index][0] = reducer;
-        update(hooks[index], mount ? HOOK_MOUNT : HOOK_UPDATE);
-        return hooks[index];
-    }
-    function updated() {
-        let type = mounted ? HOOK_UPDATED : HOOK_MOUNTED;
-        mounted = 1;
-        updateAll(hooks, type);
-    }
-    function unmount() {
-        updateAll(hooks, HOOK_UNMOUNT);
-    }
-    return hook;
 }
 
 export function useState(initialState) {
