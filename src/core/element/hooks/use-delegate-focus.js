ac3cfcb63fc638f782c5a4a51a9e6bc1eec8f740
UpperCod <mtrujillo@outlook.cl>
2020-02-19 11:14:55 -0300

    delegateFocus behavior is improved

--- src/core/element/hooks/use-delegate-focus.js
+++ src/core/element/hooks/use-delegate-focus.js
@@ -1,6 +1,14 @@
 import { useHost, useMemo } from "../../hooks";
 
+const WITH_DELEGATE_FOCUS = Symbol("delegateFocus");
+
 export function useDelegateFocus(ref) {
     let { current } = useHost();
-    return useMemo(() => (current.focus = () => ref.current.focus), [ref]);
+    if (!current[WITH_DELEGATE_FOCUS]) {
+        current.tabIndex = 0;
+        current.addEventListener("focus", () => {
+            current[WITH_DELEGATE_FOCUS].current.focus();
+        });
+    }
+    current[WITH_DELEGATE_FOCUS] = ref;
 }
