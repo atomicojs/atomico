19329dbaae6a9cbff36968cf2150ece14a716861
UpperCod <mtrujillo@outlook.cl>
2020-07-19 18:29:27 -0400

    recreate the tests to run with @ web/test-runner and chai

--- src/hooks/create-hooks.js
+++ src/hooks/create-hooks.js
@@ -1,31 +1,78 @@
-export const HOOK_MOUNT = 1; //Symbol("mount");
-export const HOOK_MOUNTED = 2; //Symbol("mounted");
-export const HOOK_UPDATE = 3; //Symbol("update");
-export const HOOK_UPDATED = 4; //Symbol("updated");
-export const HOOK_UNMOUNT = 5; //Symbol("unmount");
+export const HOOK_MOUNT = 1;
+
+export const HOOK_MOUNTED = 2;
+
+export const HOOK_UPDATE = 3;
+
+export const HOOK_UPDATED = 4;
+
+export const HOOK_UNMOUNT = 5;
+
+/**
+ * @type {{index?:number,ref?:any}}
+ */
 export const HOOK_CURRENT = {};
 
+/**
+ * @template T
+ * @callback reducer
+ * @param {T} state
+ * @param {number} type
+ * @returns {T}
+ */
+
+/**
+ * @template T
+ * @typedef {[(state:T,type:number )=>T,T]} hook
+ */
+
+/**
+ * @template T
+ * @param {hook<T>} hook
+ * @param {number} type
+ */
 function update(hook, type) {
     hook[0] && (hook[1] = hook[0](hook[1], type));
 }
 
+/**
+ * @template T
+ * @param {Object.<number,hook<any>>} hooks
+ * @param {number} type
+ */
 function updateAll(hooks, type) {
     for (let i in hooks) update(hooks[i], type);
 }
-
+/**
+ * @template T
+ * @param {reducer<any>|0} reducer
+ * @param {T} [initialState]
+ * @returns {T}
+ */
 export function useHook(reducer, initialState) {
     if (HOOK_CURRENT.ref.hook) {
         return HOOK_CURRENT.ref.hook.use(reducer, initialState)[1];
     }
 }
-
+/**
+ * @returns {()=>void}
+ */
 export function useRender() {
     return HOOK_CURRENT.ref.render;
 }
-
+/**
+ *
+ * @param {()=>void} render
+ * @param {any} host
+ */
 export function createHooks(render, host) {
+    /**
+     * @type {Object.<number,hook<any>>}
+     **/
     let hooks = {};
+
     let mounted;
+
     let hook = {
         use,
         load,
@@ -34,7 +81,12 @@ export function createHooks(render, host) {
     };
 
     let ref = { hook, host, render };
-
+    /**
+     * @template T,R
+     * @param {(param:T)=>R} callback
+     * @param {T} param
+     * @returns {R}
+     */
     function load(callback, param) {
         HOOK_CURRENT.index = 0;
         HOOK_CURRENT.ref = ref;
@@ -42,6 +94,11 @@ export function createHooks(render, host) {
         HOOK_CURRENT.ref = 0;
         return resolve;
     }
+    /**
+     * @template T
+     * @param {reducer<T>} reducer
+     * @param {T} state
+     */
     function use(reducer, state) {
         let index = HOOK_CURRENT.index++;
         let mount;
@@ -52,7 +109,9 @@ export function createHooks(render, host) {
         }
         // The hook always receives the last reduce.
         hooks[index][0] = reducer;
+
         update(hooks[index], mount ? HOOK_MOUNT : HOOK_UPDATE);
+
         return hooks[index];
     }
     function updated() {
---
+++ src/hooks/tests/create-hooks.test.js
@@ -0,0 +1,62 @@
+import { expect } from "@bundled-es-modules/chai";
+import { useHook, createHooks, useRender } from "../create-hooks.js";
+
+it("hooks.load", () => {
+    function render() {}
+    let host = {};
+    let hooks = createHooks(render, host);
+    let param = {};
+    hooks.load((param) => expect(param).to.equal(param), param);
+});
+
+it("hooks > useRender", () => {
+    function render() {
+        expect;
+    }
+    let host = {};
+    let hooks = createHooks(render, host);
+    let param = {};
+    hooks.load((param) => expect(param).to.equal(param), param);
+});
+
+it("hooks > useHook", () => {
+    function render() {}
+    let host = {};
+    let hooks = createHooks(render, host);
+    let param = {};
+    hooks.load(() => {
+        useHook((param) => {
+            expect(param).to.equal(param);
+        }, param);
+    }, null);
+});
+
+it("hooks.(updated|unmount) > useHook", () => {
+    function render() {}
+    let host = {};
+    let hooks = createHooks(render, host);
+
+    let cycle = 0;
+    let steps = {};
+
+    let hooksScope = (cycle) => {
+        useHook((state, type) => {
+            (steps[cycle] = steps[cycle] || []).push(type);
+        });
+    };
+
+    let runCycle = (unmount) => {
+        hooks.load(hooksScope, cycle++);
+        unmount ? hooks.unmount() : hooks.updated();
+    };
+
+    runCycle();
+    runCycle();
+    runCycle(true);
+
+    expect(steps[0]).to.deep.equal([1, 2]); // mount - mounted
+    expect(steps[1]).to.deep.equal([3, 4]); // update - updated
+    expect(steps[2]).to.deep.equal([3, 5]); // update - unmount
+
+    expect(cycle).to.equal(3); // if it were older, additional cycles would be generated
+});
