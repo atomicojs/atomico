f508870dcd7cdcd1f9e8dbc502ed9e415e59ca6f
UpperCod <mtrujillo@outlook.cl>
2020-07-24 13:20:24 -0400

    rewrite index.d.ts

--- index.d.ts
+++ index.d.ts
@@ -1,3 +1,25 @@
+interface Host extends HTMLElement {
+    class: string;
+    shadowDom: boolean;
+    children: any;
+    [custom: string]: any;
+}
+
+type VdomTypes = typeof Node | string;
+
+interface Vdom<T, P> {
+    type: T;
+    props: P;
+    children: any[];
+    readonly key?: any;
+    readonly shadow?: boolean;
+    readonly raw?: boolean;
+}
+
 declare module "atomico" {
-    export * from "./src/core/element/element";
+    namespace JSX {
+        interface IntrinsicElements {
+            host: Partial<Host> & { style: string | Object };
+        }
+    }
 }
