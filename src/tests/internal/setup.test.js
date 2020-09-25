99033e8a8d7f60eeef5b8fd3e103364f72dfaddb
UpperCod <mtrujillo@outlook.cl>
2020-09-25 15:46:49 -0300

    add text context

--- src/tests/internal/setup.test.js
+++ src/tests/internal/setup.test.js
@@ -2,5 +2,18 @@ import { expect } from "@esm-bundle/chai";
 import { setup } from "../../element/setup";
 
 describe("internal: setup", () => {
-    it("declare basic", () => {});
+    it("setup", () => {
+        const Component = () => {};
+        class MyElement extends HTMLElement {
+            /**@this import("../../element/custom-element").BaseContext */
+            constructor() {
+                super();
+                this._props = {};
+                this.mounted = Promise.resolve();
+                this.unmounted = new Promise(() => {});
+                setup(this, Component);
+                expect(this.update).is.call;
+            }
+        }
+    });
 });
