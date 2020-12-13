bae1056cd040192a653ab03a9af57692a06f2542
UpperCod <mtrujillo@outlook.cl>
2020-12-13 17:58:47 -0300

    upgrade to setup test

--- src/tests/internal/setup.test.js
+++ src/tests/internal/setup.test.js
@@ -1,19 +1,33 @@
 import { expect } from "@esm-bundle/chai";
-import { setup } from "../../element/setup";
-
+import { setup } from "../../element/setup.js";
+import { h } from "../../render.js";
+import { customElementScope } from "../element.test.js";
 describe("internal: setup", () => {
-    it("setup", () => {
-        const Component = () => {};
+    it("setup", async () => {
+        const Component = () => h("host");
+
         class MyElement extends HTMLElement {
             /**@this import("../../element/custom-element").BaseContext */
             constructor() {
                 super();
                 this._props = {};
-                this.mounted = Promise.resolve();
-                this.unmounted = new Promise(() => {});
                 setup(this, Component);
-                expect(this.update).is.call;
+                expect(this.update).instanceOf(Function);
+                expect(this.mount).instanceOf(Function);
+                expect(this.unmount).instanceOf(Function);
+
+                this.update();
+            }
+            /**@this import("../../element/custom-element").BaseContext */
+            connectedCallback() {
+                this.mount();
             }
         }
+
+        const element = customElementScope(MyElement, false);
+
+        expect(element.mounted).instanceOf(Promise);
+        expect(element.unmounted).instanceOf(Promise);
+        expect(element.updated).instanceOf(Promise);
     });
 });
