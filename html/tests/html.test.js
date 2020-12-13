d5a7cca8f961a79b24ea65f8bcbb1bd5ef6c1eba
UpperCod <mtrujillo@outlook.cl>
2020-12-13 17:34:22 -0300

    associate test with the use of template string

---
+++ html/tests/html.test.js
@@ -0,0 +1,14 @@
+import { expect } from "@esm-bundle/chai";
+import { h } from "../../core.js";
+import { html } from "../html.js";
+
+describe("html", () => {
+    it("virtual-dom 1", () => {
+        expect(html`<host></host>`).to.deep.equal(h("host"));
+    });
+    it("virtual-dom 2", () => {
+        expect(html`<host>every</host>`).to.deep.equal(
+            h("host", null, "every")
+        );
+    });
+});
