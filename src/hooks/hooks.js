61ce72346c94cc66f46b0fd8bb337acaa4104b0b
UpperCod <mtrujillo@outlook.cl>
2020-12-28 18:12:16 -0300

    Add JSDOC, useLayoutEffect and modify useEffect

--- src/hooks/hooks.js
+++ src/hooks/hooks.js
@@ -4,12 +4,18 @@ import { isEqualArray, isFunction } from "../utils.js";
 
 export * from "./custom-hooks/use-prop.js";
 export * from "./custom-hooks/use-event.js";
-
+/**
+ * Create a persistent local state
+ * @param {*} initialState
+ */
 export function useState(initialState) {
+    // retrieve the render to request an update
     let render = useRender();
     return useHook((state = []) => {
         if (!state[1]) {
+            // Initialize the initial state
             state[0] = isFunction(initialState) ? initialState() : initialState;
+            // Associate an immutable setState to the state instance
             state[1] = (nextState) => {
                 nextState = isFunction(nextState)
                     ? nextState(state[0])
@@ -20,14 +26,45 @@ export function useState(initialState) {
                 }
             };
         }
+        // The return is always the same reference
         return state;
     });
 }
 /**
+ * Hook that is executed once the render cycle is finished by
+ * executing the return of clearEffect
  * @param {(args:any[])=>void|(()=>void)} currentEffect
  * @param {any[]} [currentArgs]
  */
 export function useEffect(currentEffect, currentArgs) {
+    useHook(
+        ([collector, args] = []) => {
+            if (args || !args) {
+                if (args && isEqualArray(args, currentArgs)) {
+                    collector = collector || true;
+                } else {
+                    if (isFunction(collector)) collector();
+                    collector = null;
+                }
+            }
+            return [collector, currentArgs];
+        },
+        null,
+        ([collector, args], unmounted) => {
+            if (unmounted) {
+                if (isFunction(collector)) collector();
+            } else {
+                return [collector ? collector : currentEffect(args), args];
+            }
+        }
+    );
+}
+/**
+ * Hook that is executed once the render cycle has been completed by executing clearEffect
+ * @param {(args:any[])=>void|(()=>void)} currentEffect
+ * @param {any[]} [currentArgs]
+ */
+export function useLayoutEffect(currentEffect, currentArgs) {
     useHook(
         ([collector, args] = []) => {
             if (args || !args) {
@@ -49,8 +86,8 @@ export function useEffect(currentEffect, currentArgs) {
         }
     );
 }
-
 /**
+ * Create a persistent reference
  * @template T
  * @param {T} [current]
  * @returns {{current:T}}
@@ -60,6 +97,7 @@ export function useRef(current) {
 }
 
 /**
+ * Memorize the return of a callback
  * @template T
  * @param {(args:any[])=>T} currentMemo
  * @param {any[]} [currentArgs]
@@ -74,7 +112,11 @@ export function useMemo(currentMemo, currentArgs) {
     });
     return state;
 }
-
+/**
+ * Apply the redux pattern as a hook
+ * @param {(state:any,action:any)=>any} reducer
+ * @param {any} initialState
+ */
 export function useReducer(reducer, initialState) {
     let render = useRender();
     return useHook((state = []) => {
@@ -92,6 +134,8 @@ export function useReducer(reducer, initialState) {
     });
 }
 /**
+ * Memorize a callback allowing it to remember the scope
+ * variables regardless of the render
  * @template {()=>any} T;
  * @param {T} callback
  * @param {any[]} [args]
