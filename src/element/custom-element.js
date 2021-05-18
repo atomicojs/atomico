0ceee574e1c7d5cdea7de9529e41b0ba1cc2f338
UpperCod <mtrujillo@outlook.cl>
2021-05-17 23:09:28 -0400

    Add support for static styles

--- src/element/custom-element.js
+++ src/element/custom-element.js
@@ -1,8 +1,10 @@
 import { setPrototype, transformValue } from "./set-prototype.js";
 import { createHooks } from "../hooks/create-hooks.js";
-import { render } from "../render.js";
+import { render, $ } from "../render.js";
 export { Any } from "./set-prototype.js";
 
+//@ts-ignore
+export const SupportAdoptedStyleSheets = !!$.adoptedStyleSheets;
 /**
  * Class to extend for lifecycle assignment
  * @param {any} component - Function to transform into customElement
@@ -18,7 +20,7 @@ export function c(component, Base = HTMLElement) {
      */
     let values = {};
 
-    let { props } = component;
+    let { props, styles } = component;
 
     let Atom = class extends Base {
         constructor() {
@@ -26,6 +28,20 @@ export function c(component, Base = HTMLElement) {
             this._setup();
             this._render = () => component({ ...this._props });
             for (let prop in values) this[prop] = values[prop];
+        }
+        /**
+         * @returns {Style[]|Style}
+         */
+        static get styles() {
+            return styles;
+        }
+        /**
+         * @returns {Style[]|Style}
+         */
+        static get _styles() {
+            let { styles } = this;
+            //@ts-ignore
+            return [].concat(super.styles || [], styles || []);
         }
 
         async _setup() {
@@ -76,6 +92,11 @@ export function c(component, Base = HTMLElement) {
             };
 
             this.update();
+
+            await this.updated;
+
+            // @ts-ignore
+            applyStyles(this);
 
             await this.unmounted;
 
@@ -129,6 +150,31 @@ export function c(component, Base = HTMLElement) {
 
     return Atom;
 }
+/**
+ * Attach the css to the shadowDom
+ * @param {Base &  {shadowRoot: ShadowRoot, constructor: {_styles: Style[] }} } host
+ */
+function applyStyles(host) {
+    let { _styles = [] } = host.constructor;
+    let { shadowRoot } = host;
+    if (shadowRoot && _styles.length) {
+        if (SupportAdoptedStyleSheets) {
+            //@ts-ignore
+            shadowRoot.adoptedStyleSheets = _styles;
+        } else {
+            _styles.map((style) =>
+                // @ts-ignore
+                shadowRoot.appendChild(style.cloneNode(true))
+            );
+        }
+    }
+}
+
+/**
+ * @typedef {Object} ShadowRoot
+ * @property {CSSStyleSheet[]} [adoptedStyleSheets]
+ * @property {(child:ChildNode)=>void} appendChild
+ */
 
 /**
  * @typedef {typeof HTMLElement} Base
@@ -145,6 +191,10 @@ export function c(component, Base = HTMLElement) {
  * @property {Object<string,any>} _props
  * @property {string} [_ignoreAttr]
  * @property {symbol} [symbolId]  - symbolId allows to obtain the symbol id that stores the state of the virtual-dom
+ */
+
+/**
+ * @typedef {CSSStyleSheet|HTMLStyleElement} Style
  */
 
 /**
