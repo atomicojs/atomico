6ae28d1d80010c6cc33fb7ad4bf0a78ef3a5d918
UpperCod <mtrujillo@outlook.cl>
2021-05-18 11:56:24 -0400

    Add the error export

--- core.js
+++ core.js
@@ -1,5 +1,6 @@
 export * from "./src/element/custom-element.js";
 export * from "./src/hooks/hooks.js";
+export * from "./src/element/errors.js";
 export { useHost, useUpdate } from "./src/hooks/create-hooks.js";
 export { render, h, Mark } from "./src/render.js";
 export { css } from "./src/css.js";
