155bdf8a8a7b0865e61070f9a1d31b1233623eeb
UpperCod <mtrujillo@outlook.cl>
2021-06-30 23:44:07 -0400

    add export options

--- src/core.js
+++ src/core.js
@@ -1,6 +1,7 @@
 export * from "./element/custom-element.js";
 export * from "./hooks/hooks.js";
 export * from "./element/errors.js";
+export * from "./options.js";
 export { useHost, useUpdate } from "./hooks/create-hooks.js";
 export { render, h, Mark } from "./render.js";
 export { css } from "./css.js";
