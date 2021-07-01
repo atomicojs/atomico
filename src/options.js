121a3b8d54f400ed42856cecef69126588f67f9c
UpperCod <mtrujillo@outlook.cl>
2021-06-30 23:45:05 -0400

    now options is exported at the core level, defualt is removed and the SSR property is added

--- src/options.js
+++ src/options.js
@@ -1,9 +1,13 @@
-/**
- * @type {{sheet:boolean}}
- */
-const options = {
+export const options = {
+    /**
+     * CSSStyleSheet support
+     * @type {boolean}
+     */
     //@ts-ignore
     sheet: !!document.adoptedStyleSheets,
+    /**
+     * define if you use rendering from the server
+     * @type {boolean}
+     */
+    ssr: false,
 };
-
-export default options;
