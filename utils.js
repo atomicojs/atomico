efffb76222810e4ddcc2564a1efe1072320b6118
UpperCod <mtrujillo@outlook.cl>
2022-01-01 03:07:59 -0300

    add template

--- utils.js
+++ utils.js
@@ -10,10 +10,27 @@ const compatibilityList = [
     ["for", w.Symbol],
 ];
 
+/**
+ * serialize a string
+ * @param  {...any} args
+ * @returns {string}
+ */
 export const serialize = (...args) => args.filter((value) => value).join(" ");
 
+/**
+ * check the features that Atomico leverages the browser
+ * @returns {string[]}
+ */
 export const checkIncompatibility = () =>
     compatibilityList
         //@ts-ignore
         .map(([check, ctx]) => (!ctx || !(check in ctx) ? check : 0))
         .filter((check) => check);
+
+/**
+ * @template {Element} T
+ * @param {*} vnode
+ * @returns {T}
+ */
+export const template = (vnode) =>
+    render(h("host", null, vnode), new DocumentFragment()).firstElementChild;
