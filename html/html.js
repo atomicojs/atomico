65d4bf576c531440fe0cb056ddce3c83789cd7bd
UpperCod <mtrujillo@outlook.cl>
2022-03-05 02:29:56 -0300

    compression

--- html/html.js
+++ html/html.js
@@ -1,6 +1,6 @@
 import htm from "htm";
 import { h } from "../src/render.js";
 
-export const html = htm.bind(h);
+export let html = htm.bind(h);
 
 export default html;
--- utils.js
+++ utils.js
@@ -1,13 +1,13 @@
-const w = window;
+let W = window;
 
-const compatibilityList = [
-    ["customElements", w],
-    ["ShadowRoot", w],
-    ["Map", w],
+let COMPATIBILITY_LIST = [
+    ["customElements", W],
+    ["ShadowRoot", W],
+    ["Map", W],
     ["append", document],
     ["prepend", document],
-    ["Symbol", w],
-    ["for", w.Symbol],
+    ["Symbol", W],
+    ["for", W.Symbol],
 ];
 
 /**
@@ -15,14 +15,14 @@ const compatibilityList = [
  * @param  {...any} args
  * @returns {string}
  */
-export const serialize = (...args) => args.filter((value) => value).join(" ");
+export let serialize = (...args) => args.filter((value) => value).join(" ");
 
 /**
  * check the features that Atomico leverages the browser
  * @returns {string[]}
  */
-export const checkIncompatibility = () =>
-    compatibilityList
+export let checkIncompatibility = () =>
+    COMPATIBILITY_LIST
         //@ts-ignore
         .map(([check, ctx]) => (!ctx || !(check in ctx) ? check : 0))
         .filter((check) => check);
