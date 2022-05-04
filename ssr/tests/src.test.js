3855844726b7a7c0d69657fbb8fecce83cdb0571
UpperCod <mtrujillo@outlook.cl>
2022-05-04 17:30:07 -0400

    fix shadowroot hydration

--- ssr/tests/src.test.js
+++ ssr/tests/src.test.js
@@ -3,7 +3,7 @@
 
     it("SSR", async () => {
         const root = document.createElement("div");
-        root.innerHTML = `<my-component data-hydrate range="100" class="random" ><span>1</span><span> </span><span>2</span><span> </span><span>3</span><button><span>ok</span></button><a data-hydrate count="0" data="{&quot;ok&quot;:1}" is="my-component-3" ><button><span>increment</span></button><span><span>0</span></span><button><span>decrement</span></button></a><my-component-2 data-hydrate count="10" ><template shadow-dom ><button><span>increment</span></button><span><span>10</span></span><button><span>decrement</span></button></template></my-component-2><h1 slot="random" ><span>....</span></h1><span>1</span><span> </span><span>2</span><span> </span><span>3</span></my-component>`;
+        root.innerHTML = `<my-component data-hydrate range="100" class="random" ><span>1</span><span> </span><span>2</span><span> </span><span>3</span><button><span>ok</span></button><a data-hydrate count="0" data="{&quot;ok&quot;:1}" is="my-component-3" ><button><span>increment</span></button><span><span>0</span></span><button><span>decrement</span></button></a><my-component-2 data-hydrate count="10" ><template shadowroot="open" ><button><span>increment</span></button><span><span>10</span></span><button><span>decrement</span></button></template></my-component-2><h1 slot="random" ><span>....</span></h1><span>1</span><span> </span><span>2</span><span> </span><span>3</span></my-component>`;
 
         document.body.append(root);
 
