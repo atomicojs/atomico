270b45d80687a6119c4cf587d0a74bbda3d2708f
UpperCod <mtrujillo@outlook.cl>
2022-05-06 03:15:08 -0400

    fix the use of htm as module

--- html.js
+++ html.js
@@ -1,120 +1,233 @@
-import { h } from "./src/render.js";
+import { h } from './src/render.js';
 
-var n = function (t, s, r, e) {
-        var u;
-        s[0] = 0;
-        for (var h = 1; h < s.length; h++) {
-            var p = s[h++],
-                a = s[h] ? ((s[0] |= p ? 1 : 2), r[s[h++]]) : s[++h];
-            3 === p
-                ? (e[0] = a)
-                : 4 === p
-                ? (e[1] = Object.assign(e[1] || {}, a))
-                : 5 === p
-                ? ((e[1] = e[1] || {})[s[++h]] = a)
-                : 6 === p
-                ? (e[1][s[++h]] += a + "")
-                : p
-                ? ((u = t.apply(a, n(t, a, r, ["", null]))),
-                  e.push(u),
-                  a[0] ? (s[0] |= 2) : ((s[h - 2] = 0), (s[h] = u)))
-                : e.push(a);
-        }
-        return e;
-    },
-    t = new Map();
-function htm(s) {
-    var r = t.get(this);
-    return (
-        r || ((r = new Map()), t.set(this, r)),
-        (r = n(
-            this,
-            r.get(s) ||
-                (r.set(
-                    s,
-                    (r = (function (n) {
-                        for (
-                            var t,
-                                s,
-                                r = 1,
-                                e = "",
-                                u = "",
-                                h = [0],
-                                p = function (n) {
-                                    1 === r &&
-                                    (n ||
-                                        (e = e.replace(
-                                            /^\s*\n\s*|\s*\n\s*$/g,
-                                            ""
-                                        )))
-                                        ? h.push(0, n, e)
-                                        : 3 === r && (n || e)
-                                        ? (h.push(3, n, e), (r = 2))
-                                        : 2 === r && "..." === e && n
-                                        ? h.push(4, n, 0)
-                                        : 2 === r && e && !n
-                                        ? h.push(5, 0, !0, e)
-                                        : r >= 5 &&
-                                          ((e || (!n && 5 === r)) &&
-                                              (h.push(r, 0, e, s), (r = 6)),
-                                          n && (h.push(r, n, 0, s), (r = 6))),
-                                        (e = "");
-                                },
-                                a = 0;
-                            a < n.length;
-                            a++
-                        ) {
-                            a && (1 === r && p(), p(a));
-                            for (var l = 0; l < n[a].length; l++)
-                                (t = n[a][l]),
-                                    1 === r
-                                        ? "<" === t
-                                            ? (p(), (h = [h]), (r = 3))
-                                            : (e += t)
-                                        : 4 === r
-                                        ? "--" === e && ">" === t
-                                            ? ((r = 1), (e = ""))
-                                            : (e = t + e[0])
-                                        : u
-                                        ? t === u
-                                            ? (u = "")
-                                            : (e += t)
-                                        : '"' === t || "'" === t
-                                        ? (u = t)
-                                        : ">" === t
-                                        ? (p(), (r = 1))
-                                        : r &&
-                                          ("=" === t
-                                              ? ((r = 5), (s = e), (e = ""))
-                                              : "/" === t &&
-                                                (r < 5 || ">" === n[a][l + 1])
-                                              ? (p(),
-                                                3 === r && (h = h[0]),
-                                                (r = h),
-                                                (h = h[0]).push(2, 0, r),
-                                                (r = 0))
-                                              : " " === t ||
-                                                "\t" === t ||
-                                                "\n" === t ||
-                                                "\r" === t
-                                              ? (p(), (r = 2))
-                                              : (e += t)),
-                                    3 === r &&
-                                        "!--" === e &&
-                                        ((r = 4), (h = h[0]));
-                        }
-                        return p(), h;
-                    })(s))
-                ),
-                r),
-            arguments,
-            []
-        )).length > 1
-            ? r
-            : r[0]
-    );
-}
+const MODE_SLASH = 0;
+const MODE_TEXT = 1;
+const MODE_WHITESPACE = 2;
+const MODE_TAGNAME = 3;
+const MODE_COMMENT = 4;
+const MODE_PROP_SET = 5;
+const MODE_PROP_APPEND = 6;
 
-const html = htm.bind(h);
+const CHILD_APPEND = 0;
+const CHILD_RECURSE = 2;
+const TAG_SET = 3;
+const PROPS_ASSIGN = 4;
+const PROP_SET = MODE_PROP_SET;
+const PROP_APPEND = MODE_PROP_APPEND;
+
+const evaluate = (h, built, fields, args) => {
+	let tmp;
+
+	// `build()` used the first element of the operation list as
+	// temporary workspace. Now that `build()` is done we can use
+	// that space to track whether the current element is "dynamic"
+	// (i.e. it or any of its descendants depend on dynamic values).
+	built[0] = 0;
+
+	for (let i = 1; i < built.length; i++) {
+		const type = built[i++];
+
+		// Set `built[0]`'s appropriate bits if this element depends on a dynamic value.
+		const value = built[i] ? ((built[0] |= type ? 1 : 2), fields[built[i++]]) : built[++i];
+
+		if (type === TAG_SET) {
+			args[0] = value;
+		}
+		else if (type === PROPS_ASSIGN) {
+			args[1] = Object.assign(args[1] || {}, value);
+		}
+		else if (type === PROP_SET) {
+			(args[1] = args[1] || {})[built[++i]] = value;
+		}
+		else if (type === PROP_APPEND) {
+			args[1][built[++i]] += (value + '');
+		}
+		else if (type) { // type === CHILD_RECURSE
+			// Set the operation list (including the staticness bits) as
+			// `this` for the `h` call.
+			tmp = h.apply(value, evaluate(h, value, fields, ['', null]));
+			args.push(tmp);
+
+			if (value[0]) {
+				// Set the 2nd lowest bit it the child element is dynamic.
+				built[0] |= 2;
+			}
+			else {
+				// Rewrite the operation list in-place if the child element is static.
+				// The currently evaluated piece `CHILD_RECURSE, 0, [...]` becomes
+				// `CHILD_APPEND, 0, tmp`.
+				// Essentially the operation list gets optimized for potential future
+				// re-evaluations.
+				built[i-2] = CHILD_APPEND;
+				built[i] = tmp;
+			}
+		}
+		else { // type === CHILD_APPEND
+			args.push(value);
+		}
+	}
+
+	return args;
+};
+
+const build = function(statics) {
+
+	let mode = MODE_TEXT;
+	let buffer = '';
+	let quote = '';
+	let current = [0];
+	let char, propName;
+
+	const commit = field => {
+		if (mode === MODE_TEXT && (field || (buffer = buffer.replace(/^\s*\n\s*|\s*\n\s*$/g,'')))) {
+			{
+				current.push(CHILD_APPEND, field, buffer);
+			}
+		}
+		else if (mode === MODE_TAGNAME && (field || buffer)) {
+			{
+				current.push(TAG_SET, field, buffer);
+			}
+			mode = MODE_WHITESPACE;
+		}
+		else if (mode === MODE_WHITESPACE && buffer === '...' && field) {
+			{
+				current.push(PROPS_ASSIGN, field, 0);
+			}
+		}
+		else if (mode === MODE_WHITESPACE && buffer && !field) {
+			{
+				current.push(PROP_SET, 0, true, buffer);
+			}
+		}
+		else if (mode >= MODE_PROP_SET) {
+			{
+				if (buffer || (!field && mode === MODE_PROP_SET)) {
+					current.push(mode, 0, buffer, propName);
+					mode = MODE_PROP_APPEND;
+				}
+				if (field) {
+					current.push(mode, field, 0, propName);
+					mode = MODE_PROP_APPEND;
+				}
+			}
+		}
+
+		buffer = '';
+	};
+
+	for (let i=0; i<statics.length; i++) {
+		if (i) {
+			if (mode === MODE_TEXT) {
+				commit();
+			}
+			commit(i);
+		}
+
+		for (let j=0; j<statics[i].length;j++) {
+			char = statics[i][j];
+
+			if (mode === MODE_TEXT) {
+				if (char === '<') {
+					// commit buffer
+					commit();
+					{
+						current = [current];
+					}
+					mode = MODE_TAGNAME;
+				}
+				else {
+					buffer += char;
+				}
+			}
+			else if (mode === MODE_COMMENT) {
+				// Ignore everything until the last three characters are '-', '-' and '>'
+				if (buffer === '--' && char === '>') {
+					mode = MODE_TEXT;
+					buffer = '';
+				}
+				else {
+					buffer = char + buffer[0];
+				}
+			}
+			else if (quote) {
+				if (char === quote) {
+					quote = '';
+				}
+				else {
+					buffer += char;
+				}
+			}
+			else if (char === '"' || char === "'") {
+				quote = char;
+			}
+			else if (char === '>') {
+				commit();
+				mode = MODE_TEXT;
+			}
+			else if (!mode) ;
+			else if (char === '=') {
+				mode = MODE_PROP_SET;
+				propName = buffer;
+				buffer = '';
+			}
+			else if (char === '/' && (mode < MODE_PROP_SET || statics[i][j+1] === '>')) {
+				commit();
+				if (mode === MODE_TAGNAME) {
+					current = current[0];
+				}
+				mode = current;
+				{
+					(current = current[0]).push(CHILD_RECURSE, 0, mode);
+				}
+				mode = MODE_SLASH;
+			}
+			else if (char === ' ' || char === '\t' || char === '\n' || char === '\r') {
+				// <a disabled>
+				commit();
+				mode = MODE_WHITESPACE;
+			}
+			else {
+				buffer += char;
+			}
+
+			if (mode === MODE_TAGNAME && buffer === '!--') {
+				mode = MODE_COMMENT;
+				current = current[0];
+			}
+		}
+	}
+	commit();
+	return current;
+};
+
+/**
+ * Copyright 2018 Google Inc. All Rights Reserved.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+const CACHES = new Map();
+
+const regular = function(statics) {
+	let tmp = CACHES.get(this);
+	if (!tmp) {
+		tmp = new Map();
+		CACHES.set(this, tmp);
+	}
+	tmp = evaluate(this, tmp.get(statics) || (tmp.set(statics, tmp = build(statics)), tmp), arguments, []);
+	return tmp.length > 1 ? tmp : tmp[0];
+};
+
+var htm = regular;
+
+let html = htm.bind(h);
 
 export { html };
--- html/html.js
+++ html/html.js
@@ -1,4 +1,4 @@
-import htm from "htm";
+import htm from "htm/src/index.mjs";
 import { h } from "../src/render.js";
 
 export let html = htm.bind(h);
