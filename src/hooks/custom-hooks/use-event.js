003e0f19073c4f0c7b9ec8200b4e6bc8f2bc30ff
UpperCod <uppercod.git@gmail.com>
2022-08-18 10:42:33 -0400

    JSDOC

--- src/hooks/custom-hooks/use-event.js
+++ src/hooks/custom-hooks/use-event.js
@@ -2,8 +2,7 @@ import { useHost } from "../create-hooks.js";
 import { dispatchEvent } from "../../element/set-prototype.js";
 
 /**
- *
- * @type {import("atomico").UseEvent}
+ * @type {import("core").UseEvent}
  */
 export let useEvent = (type, eventInit = {}) => {
     let ref = useHost();
