53eb848b507cc3a8181bb118389bd734d3633f0c
UpperCod <uppercod.git@gmail.com>
2022-11-08 16:44:10 -0300

    new path and limits the export of context.js

--- src/core.js
+++ src/core.js
@@ -1,9 +1,10 @@
 export * from "./element/custom-element.js";
 export * from "./hooks/hooks.js";
+export * from "./hooks/custom-hooks.js";
 export * from "./element/errors.js";
 export * from "./options.js";
 export { useHook, useHost, useRef, useUpdate } from "./hooks/create-hooks.js";
 export { render, h, Mark, Fragment } from "./render.js";
 export { template } from "./template.js";
 export { css } from "./css.js";
-export * from "./context.js";
+export { useContext, createContext } from "./context.js";
