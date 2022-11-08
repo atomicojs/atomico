89aa07108ef92b8812182065a80ff55157d25c94
UpperCod <uppercod.git@gmail.com>
2022-11-08 16:44:59 -0300

    TS

--- src/hooks/custom-hooks/use-prop.js
+++ src/hooks/custom-hooks/use-prop.js
@@ -8,6 +8,9 @@ export let useProp = (name) => {
     let ref = useHost();
     if (name in ref.current) {
         if (!ref[name]) {
+            /**
+             * @type {import("core").ReturnUseProp<any>}
+             */
             let updater = [
                 null,
                 (nextValue) => (ref.current[name] = nextValue),
@@ -15,6 +18,7 @@ export let useProp = (name) => {
             ref[name] = updater;
         }
         ref[name][0] = ref.current[name];
+
         return ref[name];
     }
 };
