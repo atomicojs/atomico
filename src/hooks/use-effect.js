55db115fe15220a5f58a80fbfd1ec2c3755504e5
UpperCod <uppercod.git@gmail.com>
2022-11-08 16:41:33 -0300

    simplifies effects and now allows tagging

--- src/hooks/use-effect.js
+++ src/hooks/use-effect.js
@@ -1,34 +1,24 @@
-import { useHook } from "./create-hooks.js";
+import {
+    useHook,
+    IdLayoutEffect,
+    IdEffect,
+    IdInsertionEffect,
+} from "./create-hooks.js";
 import { isEqualArray, isFunction } from "../utils.js";
 
 /**
  * useLayoutEffect and useEffect have a similar algorithm
  * in that the position of the callback varies.
- * @param {1|2} type - 1 = useLayouteffect, 2 = useEffect
+ * @param {IdLayoutEffect|IdEffect|IdInsertionEffect} type
+ * @return {import("internal/hooks").UseAnyEffect}
  */
 let createEffect = (type) => (currentEffect, currentArgs) => {
-    /**
-     * clear or initialize the effect hook
-     * @param {[Collector|boolean,any]} state
-     * @param {*} unmounted
-     */
-    let effect = ([collector, args], unmounted) => {
-        if (unmounted) {
-            // ts does not infer the following conditional
-            //@ts-ignore
-            isFunction(collector) && collector();
-        } else {
-            return [collector ? collector : currentEffect(), args];
-        }
-    };
-
     useHook(
         /**
          * Clean the effect hook
-         * @param {[Collector|boolean,any[]]} state
+         * @type {import("internal/hooks").CollectorEffect}
          */
-        // TS does not infer the optional parameter
-        // @ts-ignore
+
         ([collector, args] = []) => {
             if (args || !args) {
                 if (args && isEqualArray(args, currentArgs)) {
@@ -43,18 +33,23 @@ let createEffect = (type) => (currentEffect, currentArgs) => {
             return [collector, currentArgs];
         },
         /**
-         * The following block is internal to compress code by reusing
-         * the same logic for useLayoutEffect and useEffect
+         * @returns {any}
          */
-        type == 1 && effect,
-        type == 2 && effect
+        ([collector, args], unmounted) => {
+            if (unmounted) {
+                // ts does not infer the following conditional
+                isFunction(collector) && collector();
+                return [];
+            } else {
+                return [collector ? collector : currentEffect(), args];
+            }
+        },
+        type
     );
 };
 
-export let useLayoutEffect = createEffect(1);
+export let useLayoutEffect = createEffect(IdLayoutEffect);
 
-export let useEffect = createEffect(2);
+export let useEffect = createEffect(IdEffect);
 
-/**
- * @callback Collector
- */
+export let useInsertionEffect = createEffect(IdInsertionEffect);
