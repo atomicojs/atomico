e52c4cd50b28ef4f2e2a658c8bcea7230f5a0ea9
UpperCod <uppercod.git@gmail.com>
2024-03-31 00:50:33 -0300

    feat: prevents attaching listener if document is already loaded

--- src/loaded.js
+++ src/loaded.js
@@ -2,5 +2,11 @@ import { options } from "./options.js";
 import { addListener } from "./utils.js";
 
 export const DOMLoaded = new Promise((resolve) => {
-    if (!options.ssr) addListener(globalThis, "DOMContentLoaded", resolve);
+    if (!options.ssr) {
+        if (document.readyState === "loading") {
+            addListener(document, "DOMContentLoaded", resolve);
+        } else {
+            resolve();
+        }
+    }
 });
