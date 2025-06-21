6560db23835b8fcf1ad9fed1dd693bcfbca83b00
UpperCod <uppercod.git@gmail.com>
2025-06-20 23:01:55 -0400

    dx: validity usage example

--- developer-experience/hooks/src/use-internals.tsx
+++ developer-experience/hooks/src/use-internals.tsx
@@ -1,26 +1,39 @@
-import { c, useFormProps, useFormValidity, useRef, useState } from "atomico";
+import {
+    c,
+    useFormProps,
+    useFormValidity,
+    useRef,
+    useState,
+    css
+} from "atomico";
 import { delegateValidity } from "atomico/utils";
 
 const MyInput = c(
-    () => {
+    ({ name }) => {
         const [value = "", setValue] = useFormProps();
         const ref = useRef<HTMLInputElement>();
-        const [message, validity] = useFormValidity(
+        const [, validity] = useFormValidity(
             () => delegateValidity(ref.current),
             [value]
         );
 
         return (
             <host shadowDom={{ delegatesFocus: true }}>
-                <input
-                    value={value}
-                    ref={ref}
-                    oninput={({ currentTarget }) => {
-                        setValue(currentTarget.value);
-                    }}
-                    required
-                    minLength={3}
-                />
+                <label>
+                    <span>
+                        {!validity.valid && "🔴"}
+                        {name}
+                    </span>
+                    <input
+                        value={value}
+                        ref={ref}
+                        oninput={({ currentTarget }) => {
+                            setValue(currentTarget.value);
+                        }}
+                        required
+                        minLength={3}
+                    />
+                </label>
             </host>
         );
     },
@@ -29,32 +42,51 @@ const MyInput = c(
         props: {
             name: String,
             value: String
-        }
+        },
+        styles: css`
+            :host {
+                display: block;
+            }
+            label {
+                display: grid;
+                padding: 0.5rem 0;
+                box-sizing: border-box;
+            }
+        `
     }
 );
 
-const MyLogin = c(() => {
-    const [data, setData] = useState();
-    return (
-        <host>
-            <form
-                oninput={({ currentTarget }) => {
-                    setData(
-                        Object.fromEntries(
-                            new FormData(currentTarget as HTMLFormElement)
-                        )
-                    );
-                }}
-            >
-                <MyInput name="user" />
-                <MyInput name="password" />
-                <button type="submit">Login</button>
-            </form>
-            <hr />
-            <code>{JSON.stringify(data)}</code>
-        </host>
-    );
-});
+const MyLogin = c(
+    () => {
+        const [data, setData] = useState();
+        return (
+            <host shadowDom>
+                <form
+                    oninput={({ currentTarget }) => {
+                        setData(
+                            Object.fromEntries(
+                                new FormData(currentTarget as HTMLFormElement)
+                            )
+                        );
+                    }}
+                >
+                    <MyInput name="user" />
+                    <MyInput name="password" />
+                    <button type="submit">Login</button>
+                </form>
+                <hr />
+                <code>{JSON.stringify(data)}</code>
+            </host>
+        );
+    },
+    {
+        styles: css`
+            :host {
+                display: grid;
+            }
+        `
+    }
+);
 
 customElements.define("my-input", MyInput);
 customElements.define("my-login", MyLogin);
