880cd794a18dda328755976b10132a69f7b162c1
Matias Trujillo <uppercod.git@gmail.com>
2026-06-02 23:46:17 -0400

    fix: use-event - It will only propagate to a callback if the event was declared using event

--- src/hooks/custom-hooks/use-event.js
+++ src/hooks/custom-hooks/use-event.js
@@ -1,5 +1,6 @@
 import { useHost } from "../create-hooks.js";
-import { dispatchEvent } from "../../element/set-prototype.js";
+import { dispatchEvent, SYMBOL_EVENT } from "../../element/set-prototype.js";
+import { isFunction } from "../../utils.js";
 
 /**
  * @type {import("core").UseEvent}
@@ -8,8 +9,9 @@ export const useEvent = (type, eventInit = {}) => {
     const ref = useHost();
     if (!ref[type]) {
         ref[type] = (detail = eventInit.detail) => {
-            if (type in ref.current) {
-                ref.current[type](detail);
+            const target = ref.current[type];
+            if (isFunction(target) && (target[SYMBOL_EVENT] || type === "click")) {
+                target(detail);
             } else {
                 dispatchEvent(ref.current, {
                     type,
